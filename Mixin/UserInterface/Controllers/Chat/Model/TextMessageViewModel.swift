b5b17e696d78f32003945de2103ebd0827fbc292
wuyueyang <wuyueyang@shou.tv>
2018-05-04 20:40:25 +0800

    Fixed corrupted layout for text messages of empty strings

--- Mixin/UserInterface/Controllers/Chat/Model/TextMessageViewModel.swift
+++ Mixin/UserInterface/Controllers/Chat/Model/TextMessageViewModel.swift
@@ -23,6 +23,7 @@ class TextMessageViewModel: DetailInfoMessageViewModel {
     private let labelMargin = Margin(leading: 16, trailing: 10, top: 7, bottom: 7)
     private let timeLeftMargin: CGFloat = 20
     
+    private let minimumTextSize = CGSize(width: 5, height: 18)
     private let font = UIFont.systemFont(ofSize: 16)
     private let linkColor = UIColor.systemTint
     private let textAlignment = CTTextAlignment.left
@@ -84,8 +85,10 @@ class TextMessageViewModel: DetailInfoMessageViewModel {
         let maxLabelWidth = layoutWidth - MessageViewModel.backgroundImageMargin.horizontal - labelMargin.horizontal
         let framesetter = CTFramesetterCreateWithAttributedString(str as CFAttributedString)
         let layoutSize = CGSize(width: maxLabelWidth, height: UILayoutFittingExpandedSize.height)
-        var textSize = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, .zero, nil, layoutSize, nil)
-        textSize = CGSize(width: ceil(textSize.width), height: ceil(textSize.height))
+        textSize = ceil(CTFramesetterSuggestFrameSizeWithConstraints(framesetter, .zero, nil, layoutSize, nil))
+        if textSize.height < minimumTextSize.height {
+            textSize = minimumTextSize
+        }
         let path = CGPath(rect: CGRect(origin: .zero, size: textSize), transform: nil)
         let frame = CTFramesetterCreateFrame(framesetter, .zero, path, nil)
         lines = CTFrameGetLines(frame) as! [CTLine]
@@ -146,7 +149,6 @@ class TextMessageViewModel: DetailInfoMessageViewModel {
                 contentSize.width = min(layoutSize.width, max(contentSize.width, fullnameWidth))
             }
         }
-        self.textSize = textSize
         self.contentSize = contentSize
         didSetStyle()
     }

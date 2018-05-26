6a356d9f75ab38548c4a1a64d7452a3c2d48649e
Jun Tang <tangjun@shou.tv>
2018-05-26 20:36:32 +0800

    Save qrcode to photo library

--- Mixin/UserInterface/Windows/ReceiveMoneyWindow.swift
+++ Mixin/UserInterface/Windows/ReceiveMoneyWindow.swift
@@ -1,31 +1,15 @@
 import UIKit
 
-class ReceiveMoneyWindow: BottomSheetView {
+class ReceiveMoneyWindow: MyQRCodeWindow {
 
-    @IBOutlet weak var qrcodeAvatarImageView: AvatarImageView!
-    @IBOutlet weak var qrcodeImageView: UIImageView!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-        qrcodeAvatarImageView.layer.borderColor = UIColor.white.cgColor
-        qrcodeAvatarImageView.layer.borderWidth = 2
-        layoutIfNeeded()
+    override func render() {
         if let account = AccountAPI.shared.account {
             qrcodeImageView.image = UIImage(qrcode: "mixin://transfer/\(account.user_id)", size: qrcodeImageView.frame.width)
             qrcodeAvatarImageView.setImage(with: account)
         }
     }
 
-    @IBAction func dismissAction(_ sender: Any) {
-        dismissView()
-    }
-
-    @IBAction func moreAction(_ sender: Any) {
-
-    }
-
-    class func instance() -> ReceiveMoneyWindow {
+    override class func instance() -> ReceiveMoneyWindow {
         return Bundle.main.loadNibNamed("ReceiveMoneyWindow", owner: nil, options: nil)?.first as! ReceiveMoneyWindow
     }
 }

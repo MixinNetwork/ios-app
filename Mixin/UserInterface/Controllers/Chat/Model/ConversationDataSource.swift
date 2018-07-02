91d735b48a3f0696407b11f422367625fceda10c
wuyueyang <wuyueyang@shou.tv>
2018-07-02 15:55:29 +0800

    Fix photo loading failed

--- Mixin/UserInterface/Controllers/Chat/Model/ConversationDataSource.swift
+++ Mixin/UserInterface/Controllers/Chat/Model/ConversationDataSource.swift
@@ -222,7 +222,7 @@ class ConversationDataSource {
                 guard let tableView = self.tableView, !self.messageProcessingIsCancelled else {
                     return
                 }
-                if messages.count != 0 {
+                if !viewModels.isEmpty {
                     self.viewModels.values.flatMap({ $0 }).forEach {
                         ($0 as? TextMessageViewModel)?.removeHighlights()
                     }
@@ -236,7 +236,9 @@ class ConversationDataSource {
                         self.viewModels[date]!.append(contentsOf: newViewModels)
                     }
                 }
-                tableView.reloadData()
+                if !viewModels.isEmpty {
+                    tableView.reloadData()
+                }
                 self.isLoadingBelow = false
                 self.semaphore.signal()
             }

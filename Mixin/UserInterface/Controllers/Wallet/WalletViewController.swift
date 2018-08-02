5914ee0436f7478eee2a0111559812af7ad34f88
Jun Tang <tangjun@shou.tv>
2018-08-03 01:33:41 +0800

    Fix crash when swipe assets

--- Mixin/UserInterface/Controllers/Wallet/WalletViewController.swift
+++ Mixin/UserInterface/Controllers/Wallet/WalletViewController.swift
@@ -187,8 +187,14 @@ extension WalletViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        guard indexPath.section == 1 else {
+            return []
+        }
         return [assetAction]
     }
-    
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == 1
+    }
 }
 

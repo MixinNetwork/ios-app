21424b39955b405a34348f84c71b354a85183dc8
wuyueyang <wuyueyang@mixin.one>
2018-12-06 15:45:23 +0800

    Refactor screen size and default keyboard height

--- Mixin/UserInterface/Models/ScreenSize.swift
+++ Mixin/UserInterface/Models/ScreenSize.swift
@@ -7,6 +7,8 @@ enum ScreenSize {
     case inch4_7
     case inch5_5
     case inch5_8
+    case inch6_1
+    case inch6_5
     case unknown
     
     static let current: ScreenSize = {
@@ -22,9 +24,27 @@ enum ScreenSize {
             return .inch5_5
         case 812:
             return .inch5_8
+        case 896:
+            return UIScreen.main.scale == 3 ? .inch6_5 : .inch6_1
         default:
             return .unknown
         }
     }()
+    
+    static let defaultKeyboardHeight: CGFloat = {
+        return keyboardHeight[.current] ?? 271
+    }()
+    
+    static let minReasonableKeyboardHeight = keyboardHeight.values.min() ?? 271
+    
+    private static let keyboardHeight: [ScreenSize: CGFloat] = [
+        .inch6_5: 346,
+        .inch6_1: 346,
+        .inch5_8: 335,
+        .inch5_5: 271,
+        .inch4_7: 260,
+        .inch4: 253,
+        .inch3_5: 261
+    ]
     
 }

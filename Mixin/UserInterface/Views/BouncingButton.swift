ad550f6f47cfd30fe1b08cfaa1606b1caa7ef6d1
wuyueyang <wuyueyang@mixin.one>
2019-01-09 12:31:13 +0800

    Refactor BouncingButton

--- Mixin/UserInterface/Views/BouncingButton.swift
+++ Mixin/UserInterface/Views/BouncingButton.swift
@@ -1,24 +1,33 @@
 import UIKit
 
 class BouncingButton: UIButton {
-
-    override var isHighlighted: Bool {
-        didSet {
-            if isHighlighted && !oldValue {
-                UIView.animate(withDuration: 0.1, animations: {
-                    self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-                })
-            } else if !isHighlighted && oldValue {
-                UIView.animate(withDuration: 0.1, animations: {
-                    self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
-                }, completion: { (finished) in
-                    UIView.animate(withDuration: 0.1, animations: {
-                        self.transform = .identity
-                    })
-                })
-            }
+    
+    private let duration: TimeInterval = 0.1
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        UIView.animate(withDuration: duration) {
+            self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
         }
     }
-
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        performRestoreAnimation()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        performRestoreAnimation()
+    }
+    
+    private func performRestoreAnimation() {
+        UIView.animate(withDuration: duration, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        })
+        UIView.animate(withDuration: duration, delay: duration, options: [], animations: {
+            self.transform = .identity
+        }, completion: nil)
+    }
+    
 }
-

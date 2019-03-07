952df165b80cd4c625d79cf7feb3c6769dbc9771
wuyueyang <wuyueyang@mixin.one>
2019-03-07 12:46:45 +0800

    Add conversation accessible

---
+++ Mixin/UserInterface/Controllers/Chat/ConversationAccessible.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+protocol ConversationAccessible {
+    var conversationViewController: ConversationViewController? { get }
+}
+
+extension ConversationAccessible where Self: UIViewController {
+    
+    var conversationViewController: ConversationViewController? {
+        func findConversation(_ vc: UIViewController) -> ConversationViewController? {
+            if let vc = vc as? ConversationViewController {
+                return vc
+            } else if let parent = vc.parent {
+                return findConversation(parent)
+            } else {
+                return nil
+            }
+        }
+        if let parent = parent {
+            return findConversation(parent)
+        } else {
+            return nil
+        }
+    }
+    
+    var dataSource: ConversationDataSource? {
+        return conversationViewController?.dataSource
+    }
+    
+}

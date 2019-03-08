21f0e7eb1a13f6afa24d811b95fbb27d0783eec0
wuyueyang <wuyueyang@mixin.one>
2019-03-08 16:24:02 +0800

    Fix search box layout

--- Mixin/UserInterface/Views/ModernSearchBoxView.swift
+++ Mixin/UserInterface/Views/ModernSearchBoxView.swift
@@ -4,8 +4,16 @@ class SearchTextField: UITextField {
     
     let textMargin: CGFloat = 16
     
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
         let rect = super.textRect(forBounds: bounds)
+        return CGRect(x: rect.origin.x + textMargin,
+                      y: rect.origin.y,
+                      width: rect.width - textMargin,
+                      height: rect.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.editingRect(forBounds: bounds)
         return CGRect(x: rect.origin.x + textMargin,
                       y: rect.origin.y,
                       width: rect.width - textMargin,

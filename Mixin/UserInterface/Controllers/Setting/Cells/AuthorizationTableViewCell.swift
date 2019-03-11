09902941eb62affc7f8455cc41e20f2c79ca53e7
wuyueyang <wuyueyang@mixin.one>
2019-03-11 21:20:20 +0800

    Refactor authorizations

--- Mixin/UserInterface/Controllers/Setting/Cells/AuthorizationTableViewCell.swift
+++ Mixin/UserInterface/Controllers/Setting/Cells/AuthorizationTableViewCell.swift
@@ -7,14 +7,14 @@ class AuthorizationTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectedBackgroundView = UIView.createSelectedBackgroundView()
+    }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         iconImageView.sd_cancelCurrentImageLoad()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        separatorInset.left = titleStackView.frame.origin.x
     }
     
 }

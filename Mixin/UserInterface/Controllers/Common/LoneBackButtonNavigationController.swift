a9d1775247300dd7549946431993a874f3d81071
wuyueyang <wuyueyang@mixin.one>
2019-03-14 21:23:29 +0800

    Add LoneBackButtonNavigationController

---
+++ Mixin/UserInterface/Controllers/Common/LoneBackButtonNavigationController.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+class LoneBackButtonNavigationController: UINavigationController {
+
+    let backButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBarHidden(true, animated: false)
+        UIApplication.shared.keyWindow?.endEditing(true)
+        backButton.setImage(R.image.ic_title_back(), for: .normal)
+        backButton.addTarget(self, action: #selector(backAction(sender:)), for: .touchUpInside)
+        backButton.alpha = 0
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backButton)
+        backButton.snp.makeConstraints { (make) in
+            make.width.height.equalTo(44)
+            if #available(iOS 11.0, *) {
+                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.topMargin)
+                make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leadingMargin).offset(10)
+            } else {
+                make.top.equalTo(self.topLayoutGuide.snp.bottom)
+                make.leading.equalTo(self.view.snp.leading).offset(10)
+            }
+        }
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        defer {
+            updateBackButtonAlpha(animated: animated)
+        }
+        return super.pushViewController(viewController, animated: animated)
+    }
+    
+    @discardableResult
+    override func popViewController(animated: Bool) -> UIViewController? {
+        defer {
+            updateBackButtonAlpha(animated: animated)
+        }
+        return super.popViewController(animated: animated)
+    }
+    
+    @discardableResult
+    override func popToViewController(_ viewController: UIViewController, animated: Bool) -> [UIViewController]?  {
+        defer {
+            updateBackButtonAlpha(animated: animated)
+        }
+        return super.popToViewController(viewController, animated: animated)
+    }
+    
+    @discardableResult
+    override func popToRootViewController(animated: Bool) -> [UIViewController]?  {
+        defer {
+            updateBackButtonAlpha(animated: animated)
+        }
+        return super.popToRootViewController(animated: animated)
+    }
+    
+    @objc func backAction(sender: Any) {
+        popViewController(animated: true)
+    }
+    
+    func updateBackButtonAlpha(animated: Bool) {
+        
+    }
+    
+}
--- Mixin/UserInterface/Controllers/Login/LoginNavigationController.swift
+++ Mixin/UserInterface/Controllers/Login/LoginNavigationController.swift
@@ -1,8 +1,6 @@
 import UIKit
 
-class LoginNavigationController: UINavigationController {
-    
-    let backButton = UIButton()
+class LoginNavigationController: LoneBackButtonNavigationController {
     
     class func instance() -> LoginNavigationController {
         let vc = LoginMobileNumberViewController()
@@ -12,62 +10,10 @@ class LoginNavigationController: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigationBarHidden(true, animated: false)
-        UIApplication.shared.keyWindow?.endEditing(true)
         SignalProtocol.shared.initSignal()
-        backButton.setImage(R.image.ic_title_back(), for: .normal)
-        backButton.addTarget(self, action: #selector(backAction(sender:)), for: .touchUpInside)
-        backButton.alpha = 0
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(backButton)
-        backButton.snp.makeConstraints { (make) in
-            make.width.height.equalTo(44)
-            if #available(iOS 11.0, *) {
-                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.topMargin)
-                make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leadingMargin).offset(10)
-            } else {
-                make.top.equalTo(self.topLayoutGuide.snp.bottom)
-                make.leading.equalTo(self.view.snp.leading).offset(10)
-            }
-        }
     }
     
-    @objc func backAction(sender: Any) {
-        popViewController(animated: true)
-    }
-    
-    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-        defer {
-            updateBackButtonAlpha(animated: animated)
-        }
-        return super.pushViewController(viewController, animated: animated)
-    }
-    
-    @discardableResult
-    override func popViewController(animated: Bool) -> UIViewController? {
-        defer {
-            updateBackButtonAlpha(animated: animated)
-        }
-        return super.popViewController(animated: animated)
-    }
-    
-    @discardableResult
-    override func popToViewController(_ viewController: UIViewController, animated: Bool) -> [UIViewController]?  {
-        defer {
-            updateBackButtonAlpha(animated: animated)
-        }
-        return super.popToViewController(viewController, animated: animated)
-    }
-    
-    @discardableResult
-    override func popToRootViewController(animated: Bool) -> [UIViewController]?  {
-        defer {
-            updateBackButtonAlpha(animated: animated)
-        }
-        return super.popToRootViewController(animated: animated)
-    }
-    
-    private func updateBackButtonAlpha(animated: Bool) {
+    override func updateBackButtonAlpha(animated: Bool) {
         let alpha: CGFloat
         if viewControllers.last is LoginMobileNumberViewController || viewControllers.last is UsernameViewController {
             alpha = 0

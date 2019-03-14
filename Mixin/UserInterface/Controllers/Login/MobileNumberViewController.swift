c659b925e5437edc2c3a8d6babf6c1dd397ae5ca
wuyueyang <wuyueyang@mixin.one>
2019-03-14 14:19:07 +0800

    Change intro text color

--- Mixin/UserInterface/Controllers/Login/MobileNumberViewController.swift
+++ Mixin/UserInterface/Controllers/Login/MobileNumberViewController.swift
@@ -26,28 +26,7 @@ class MobileNumberViewController: LoginViewController {
             _ = CountryCodeLibrary.shared
         }
     }
-
-    private func updateIntro() {
-        let font = introTextView.font
-        let intro = String(format: Localized.TEXT_INTRO,
-                           Localized.BUTTON_TITLE_TERMS_OF_SERVICE,
-                           Localized.BUTTON_TITLE_PRIVACY_POLICY)
-        let nsIntro = intro as NSString
-        let fullRange = NSRange(location: 0, length: nsIntro.length)
-        let termsRange = nsIntro.range(of: Localized.BUTTON_TITLE_TERMS_OF_SERVICE)
-        let privacyRange = nsIntro.range(of: Localized.BUTTON_TITLE_PRIVACY_POLICY)
-        let attributedText = NSMutableAttributedString(string: intro)
-        let paragraphSytle = NSMutableParagraphStyle()
-        paragraphSytle.alignment = .center
-        attributedText.setAttributes([NSAttributedString.Key.paragraphStyle: paragraphSytle], range: fullRange)
-        if let font = font {
-            attributedText.addAttributes([NSAttributedString.Key.font: font], range: fullRange)
-        }
-        attributedText.addAttributes([NSAttributedString.Key.link: URL.terms], range: termsRange)
-        attributedText.addAttributes([NSAttributedString.Key.link: URL.privacy], range: privacyRange)
-        introTextView.attributedText = attributedText
-    }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if !mobileNumberTextField.isFirstResponder {
@@ -152,6 +131,31 @@ class MobileNumberViewController: LoginViewController {
                 }
             }
         }
+    }
+    
+    private func updateIntro() {
+        let font = introTextView.font
+        let intro = String(format: Localized.TEXT_INTRO,
+                           Localized.BUTTON_TITLE_TERMS_OF_SERVICE,
+                           Localized.BUTTON_TITLE_PRIVACY_POLICY)
+        let nsIntro = intro as NSString
+        let fullRange = NSRange(location: 0, length: nsIntro.length)
+        let termsRange = nsIntro.range(of: Localized.BUTTON_TITLE_TERMS_OF_SERVICE)
+        let privacyRange = nsIntro.range(of: Localized.BUTTON_TITLE_PRIVACY_POLICY)
+        let attributedText = NSMutableAttributedString(string: intro)
+        let paragraphSytle = NSMutableParagraphStyle()
+        paragraphSytle.alignment = .center
+        var attrs: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: paragraphSytle,
+            .foregroundColor: UIColor.accessoryText
+        ]
+        if let font = font {
+            attrs[.font] = font
+        }
+        attributedText.setAttributes(attrs, range: fullRange)
+        attributedText.addAttributes([NSAttributedString.Key.link: URL.terms], range: termsRange)
+        attributedText.addAttributes([NSAttributedString.Key.link: URL.privacy], range: privacyRange)
+        introTextView.attributedText = attributedText
     }
     
 }

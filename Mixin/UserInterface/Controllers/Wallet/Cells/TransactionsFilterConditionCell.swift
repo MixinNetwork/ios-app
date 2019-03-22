4813f3ad35f2769c73bc2755092a21467f0e311e
wuyueyang <wuyueyang@mixin.one>
2019-03-22 18:38:33 +0800

    Update with design

--- Mixin/UserInterface/Controllers/Wallet/Cells/TransactionsFilterConditionCell.swift
+++ Mixin/UserInterface/Controllers/Wallet/Cells/TransactionsFilterConditionCell.swift
@@ -11,12 +11,12 @@ class TransactionsFilterConditionCell: UICollectionViewCell {
         backgroundView!.clipsToBounds = true
         backgroundView!.layer.borderColor = UIColor(rgbValue: 0xE5E7EC).cgColor
         backgroundView!.layer.borderWidth = 1
-        backgroundView!.layer.cornerRadius = 6
+        backgroundView!.layer.cornerRadius = 8
         
         selectedBackgroundView = UIView()
         selectedBackgroundView!.backgroundColor = UIColor(rgbValue: 0xF5F7FA)
         selectedBackgroundView!.clipsToBounds = true
-        selectedBackgroundView!.layer.cornerRadius = 6
+        selectedBackgroundView!.layer.cornerRadius = 8
     }
     
     override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {

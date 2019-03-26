78389495e55dc73f7b514c77588822e24dbc2a1b
Jun Tang <tangjun@shou.tv>
2019-03-26 14:45:22 +0800

    Don't clear badge number when application become active

--- Mixin/AppDelegate.swift
+++ Mixin/AppDelegate.swift
@@ -102,9 +102,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         UNUserNotificationCenter.current().removeAllNotifications()
-        if UIApplication.shared.applicationIconBadgeNumber > 0 {
-            UIApplication.shared.applicationIconBadgeNumber = 0
-        }
         WebSocketService.shared.checkConnectStatus()
         cancelBackgroundTask()
 

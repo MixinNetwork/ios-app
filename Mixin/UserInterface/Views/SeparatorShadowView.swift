9e5e84a85cab15c76c1ac00661ad24dfb812b3d0
wuyueyang <wuyueyang@mixin.one>
2019-04-17 14:53:14 +0800

    Update recent used apps

--- Mixin/UserInterface/Views/SeparatorShadowView.swift
+++ Mixin/UserInterface/Views/SeparatorShadowView.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class SeparatorShadowView: UIView {
     
+    @IBInspectable var hasUpperShadow: Bool = true {
+        didSet {
+            upperShadowProviderLayer.isHidden = !hasUpperShadow
+        }
+    }
+    
     @IBInspectable var hasLowerShadow: Bool = true {
         didSet {
             lowerShadowProviderLayer.isHidden = !hasLowerShadow

7a9cbba4c67ed236969537481c99cae20cd91214
wuyueyang <wuyueyang@mixin.one>
2019-04-28 14:14:54 +0800

    Increase responding area

--- Mixin/UserInterface/Controllers/Home/Views/SearchCancelButton.swift
+++ Mixin/UserInterface/Controllers/Home/Views/SearchCancelButton.swift
@@ -3,12 +3,13 @@ import UIKit
 class SearchCancelButton: UIButton {
     
     convenience init() {
-        self.init(type: .custom)
+        self.init(type: .system)
         titleLabel?.font = .systemFont(ofSize: 16)
         setTitleColor(.highlightedText, for: .normal)
         contentEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
         setTitle(R.string.localizable.dialog_button_cancel(), for: .normal)
         sizeToFit()
+        frame.size.height = 44
     }
     
 }

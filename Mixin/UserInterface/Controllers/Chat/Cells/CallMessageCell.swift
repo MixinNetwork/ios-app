37e5b2d839da1ff575f2b5cda901b110fd699a4f
wuyueyang <wuyueyang@mixin.one>
2019-05-06 12:14:54 +0800

    Replace prefix icon for call messages

--- Mixin/UserInterface/Controllers/Chat/Cells/CallMessageCell.swift
+++ Mixin/UserInterface/Controllers/Chat/Cells/CallMessageCell.swift
@@ -2,12 +2,13 @@ import UIKit
 
 class CallMessageCell: TextMessageCell {
     
-    let prefixImageView = UIImageView(image: CallMessageViewModel.prefixImage)
+    let prefixImageView = UIImageView(frame: CGRect(origin: .zero, size: CallMessageViewModel.prefixSize))
     
     override func render(viewModel: MessageViewModel) {
         super.render(viewModel: viewModel)
         if let viewModel = viewModel as? CallMessageViewModel {
             prefixImageView.frame = viewModel.prefixFrame
+            prefixImageView.image = viewModel.prefixImage
         }
     }
     

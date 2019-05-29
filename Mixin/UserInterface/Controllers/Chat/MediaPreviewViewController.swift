4c2057696c56f9f99f6040d103aa7953ccd1b193
wuyueyang <wuyueyang@mixin.one>
2019-05-29 21:15:08 +0800

    Remove content mode setting

--- Mixin/UserInterface/Controllers/Chat/MediaPreviewViewController.swift
+++ Mixin/UserInterface/Controllers/Chat/MediaPreviewViewController.swift
@@ -6,32 +6,10 @@ class MediaPreviewViewController: UIViewController {
     
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var changeContentModeWrapperView: UIView!
-    
-    @IBOutlet weak var changeContentModeWrapperWidthConstraint: NSLayoutConstraint!
     
     private var lastRequestId: PHImageRequestID?
     private var lastAssetIdentifier: String?
     private var playerController: AVPlayerViewController?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        changeContentModeWrapperView.clipsToBounds = true
-        changeContentModeWrapperView.layer.cornerRadius = changeContentModeWrapperWidthConstraint.constant / 2
-        changeContentModeWrapperView.layer.borderColor = UIColor.white.withAlphaComponent(0.14).cgColor
-        changeContentModeWrapperView.layer.borderWidth = 1 / UIScreen.main.scale
-    }
-    
-    @IBAction func changeContentModeAction(_ sender: Any) {
-        if !imageView.isHidden {
-            let isAspectFill = imageView.contentMode == .scaleAspectFill
-            imageView.contentMode = isAspectFill ? .scaleAspectFit : .scaleAspectFill
-        }
-        if let playerController = playerController, !playerController.view.isHidden {
-            let isAspectFill = playerController.videoGravity == .resizeAspectFill
-            playerController.videoGravity = isAspectFill ? .resizeAspect : .resizeAspectFill
-        }
-    }
     
     func load(asset: PHAsset) {
         guard asset.localIdentifier != lastAssetIdentifier else {
@@ -49,9 +27,8 @@ class MediaPreviewViewController: UIViewController {
                 playerController.view.isHidden = true
             }
             imageView.image = nil
-            imageView.contentMode = .scaleAspectFill
             imageView.isHidden = false
-            PHImageManager.default().requestImage(for: asset, targetSize: imageView.bounds.size, contentMode: .aspectFill, options: nil) { [weak self] (image, _) in
+            PHImageManager.default().requestImage(for: asset, targetSize: imageView.bounds.size, contentMode: .aspectFit, options: nil) { [weak self] (image, _) in
                 DispatchQueue.main.async {
                     guard let weakSelf = self else {
                         return
@@ -65,15 +42,11 @@ class MediaPreviewViewController: UIViewController {
             imageView.isHidden = true
             if let playerController = playerController {
                 playerController.view.isHidden = false
-                UIView.performWithoutAnimation {
-                    playerController.videoGravity = .resizeAspectFill
-                }
             } else {
                 let controller = AVPlayerViewController()
                 controller.showsPlaybackControls = false
-                controller.videoGravity = .resizeAspectFill
                 addChild(controller)
-                contentView.insertSubview(controller.view, belowSubview: changeContentModeWrapperView)
+                contentView.addSubview(controller.view)
                 controller.view.snp.makeConstraints({ (make) in
                     make.edges.equalToSuperview()
                 })

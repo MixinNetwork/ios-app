93e2647bc2207293dd00376f2de3e8d164493f2c
wuyueyang <wuyueyang@mixin.one>
2019-06-20 11:55:40 +0800

    Add change number tip in home

--- Mixin/UserInterface/Controllers/Login/EmergencyContactLoginVerificationCodeViewController.swift
+++ Mixin/UserInterface/Controllers/Login/EmergencyContactLoginVerificationCodeViewController.swift
@@ -12,6 +12,9 @@ class EmergencyContactLoginVerificationCodeViewController: LoginVerificationCode
     
     override func login(code: String, registrationId: Int, keyPair: KeyUtil.RSAKeyPair) {
         EmergencyAPI.shared.verifySession(id: context.verificationId, code: code, sessionSecret: keyPair.publicKey, registrationId: registrationId) { [weak self] (result) in
+            if case .success = result {
+                HomeViewController.showChangePhoneNumberTips = true
+            }
             DispatchQueue.global().async {
                 self?.handleLoginResult(result, privateKeyPem: keyPair.privateKeyPem)
             }

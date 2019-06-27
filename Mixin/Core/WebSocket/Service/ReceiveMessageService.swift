a2865412f4e7d7b801c3faef9872988f49c2e866
Jun Tang <tangjun@shou.tv>
2019-06-27 16:32:39 +0800

    Auto download audio when redecrypt

--- Mixin/Core/WebSocket/Service/ReceiveMessageService.swift
+++ Mixin/Core/WebSocket/Service/ReceiveMessageService.swift
@@ -396,6 +396,10 @@ class ReceiveMessageService: MixinService {
                 mediaStatus = MediaStatus.CANCELED
             }
             MessageDAO.shared.updateMediaMessage(mediaData: transferMediaData, status: MessageStatus.DELIVERED.rawValue, messageId: messageId, conversationId: data.conversationId, mediaStatus: mediaStatus, messageSource: data.source)
+            if data.category == MessageCategory.SIGNAL_AUDIO.rawValue {
+                let job = AudioDownloadJob(messageId: messageId, mediaMimeType: transferMediaData.mimeType)
+                AudioJobQueue.shared.addJob(job: job)
+            }
             SendMessageService.shared.sendSessionMessage(message: Message.createMessage(mediaData: transferMediaData, data: data), data: plainText)
         case MessageCategory.SIGNAL_STICKER.rawValue:
             guard let transferStickerData = parseSticker(plainText) else {

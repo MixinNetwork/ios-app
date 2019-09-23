96d5094f8546aea837a12aadb5599466f4147916
wuyueyang <wuyueyang@mixin.one>
2019-09-23 21:16:05 +0800

    Format AppDelegate

--- Mixin/AppDelegate.swift
+++ Mixin/AppDelegate.swift
@@ -10,16 +10,18 @@ import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     static var current: AppDelegate {
         return UIApplication.shared.delegate as! AppDelegate
     }
-
+    
     let window = UIWindow(frame: UIScreen.main.bounds)
+    
+    private(set) var voipToken = ""
+    
     private var autoCanceleNotification: DispatchWorkItem?
     private var backgroundTaskID = UIBackgroundTaskIdentifier.invalid
     private var backgroundTime: Timer?
-    private(set) var voipToken = ""
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         #if RELEASE
@@ -42,95 +44,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FileManager.default.writeLog(log: "\n-----------------------\nAppDelegate...didFinishLaunching...isProtectedDataAvailable:\(UIApplication.shared.isProtectedDataAvailable)...\(Bundle.main.shortVersion)(\(Bundle.main.bundleVersion))")
         return true
     }
-
-    private func configAnalytics() {
-        guard UIApplication.shared.isProtectedDataAvailable else {
-            return
-        }
-
-        if let account = AccountAPI.shared.account {
-            Bugsnag.configuration()?.setUser(account.user_id, withName: account.full_name, andEmail: account.identity_number)
-            Crashlytics.sharedInstance().setUserIdentifier(account.user_id)
-            Crashlytics.sharedInstance().setUserName(account.full_name)
-            Crashlytics.sharedInstance().setUserEmail(account.identity_number)
-            Crashlytics.sharedInstance().setObjectValue(Bundle.main.bundleIdentifier ?? "", forKey: "Package")
-        }
-
-        CommonUserDefault.shared.checkUpdateOrInstallVersion()
-        CommonUserDefault.shared.updateFirstLaunchDateIfNeeded()
-    }
-
-    func applicationProtectedDataDidBecomeAvailable(_ application: UIApplication) {
-        guard AccountAPI.shared.account == nil else {
-            return
-        }
-        guard let account = AccountUserDefault.shared.getAccount() else {
-            return
-        }
-
-        AccountAPI.shared.account = account
-        configAnalytics()
-        if AccountAPI.shared.didLogin && !(window.rootViewController is HomeContainerViewController) {
-            checkLogin()
-        }
-    }
-
-    private func checkJailbreak() {
-        guard UIDevice.isJailbreak else {
-            return
-        }
-        Keychain.shared.clearPIN()
-    }
-
-    private func initBugsnag() {
-        guard let apiKey = MixinKeys.bugsnag else {
-            return
-        }
-        Bugsnag.start(withApiKey: apiKey)
-    }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         AudioManager.shared.pause()
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         checkServerData()
     }
-
-    private func checkServerData(isPushKit: Bool = false) {
-        guard AccountAPI.shared.didLogin else {
-            return
-        }
-        guard !DatabaseUserDefault.shared.hasUpgradeDatabase() else {
-            return
-        }
-        WebSocketService.shared.checkConnectStatus()
-
-        cancelBackgroundTask()
-        self.backgroundTaskID = UIApplication.shared.beginBackgroundTask(expirationHandler: {
-            self.cancelBackgroundTask()
-        })
-        let timeInterval: TimeInterval = !isPushKit || BlazeMessageDAO.shared.getCount() + JobDAO.shared.getCount() > 50 ? 120 : 20
-        self.backgroundTime = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: false) { (time) in
-            self.cancelBackgroundTask()
-        }
-    }
-
-    private func cancelBackgroundTask() {
-        self.backgroundTime?.invalidate()
-        self.backgroundTime = nil
-        if backgroundTaskID != .invalid {
-            UIApplication.shared.endBackgroundTask(backgroundTaskID)
-            backgroundTaskID = .invalid
-        }
-    }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         UNUserNotificationCenter.current().removeAllNotifications()
@@ -145,76 +73,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
         
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         MixinDatabase.shared.close()
         SignalDatabase.shared.close()
     }
-
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        AccountAPI.shared.updateSession(deviceToken: deviceToken.toHexString(), voip_token: "")
+    }
+    
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         completionHandler(.newData)
     }
-
-    open func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return AccountAPI.shared.didLogin && UrlWindow.checkUrl(url: url)
     }
-
-    func checkLogin() {
-        window.backgroundColor = .white
-        if AccountAPI.shared.didLogin {
-            window.rootViewController = makeInitialViewController()
-            if ContactsManager.shared.authorization == .authorized && CommonUserDefault.shared.isUploadContacts {
-                DispatchQueue.global().asyncAfter(deadline: .now() + 2, execute: {
-                    PhoneContactAPI.shared.upload(contacts: ContactsManager.shared.contacts)
-                })
-            }
-        } else {
-            if UIApplication.shared.isProtectedDataAvailable {
-                window.rootViewController = LoginNavigationController.instance()
-            } else {
-                window.rootViewController = R.storyboard.launchScreen().instantiateInitialViewController()
-            }
-        }
-        window.makeKeyAndVisible()
-    }
     
-    private func updateSharedImageCacheConfig() {
-        SDImageCacheConfig.default.maxDiskSize = 1024 * bytesPerMegaByte
-        SDImageCacheConfig.default.maxDiskAge = -1
-        SDImageCacheConfig.default.diskCacheExpireType = .accessDate
+    func applicationProtectedDataDidBecomeAvailable(_ application: UIApplication) {
+        guard AccountAPI.shared.account == nil else {
+            return
+        }
+        guard let account = AccountUserDefault.shared.getAccount() else {
+            return
+        }
+
+        AccountAPI.shared.account = account
+        configAnalytics()
+        if AccountAPI.shared.didLogin && !(window.rootViewController is HomeContainerViewController) {
+            checkLogin()
+        }
     }
     
 }
 
 extension AppDelegate: PKPushRegistryDelegate {
-
+    
     func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
         voipToken = pushCredentials.token.toHexString()
         if AccountAPI.shared.didLogin {
             AccountAPI.shared.updateSession(deviceToken: "", voip_token: voipToken)
         }
     }
-
+    
     func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
         checkServerData(isPushKit: true)
     }
-
+    
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
-
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        AccountAPI.shared.updateSession(deviceToken: deviceToken.toHexString(), voip_token: "")
-    }
-
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         if !handerQuickAction(response) {
             dealWithRemoteNotification(response.notification.request.content.userInfo)
         }
         completionHandler()
     }
-
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         let userInfo = notification.request.content.userInfo
         if userInfo["fromWebSocket"] as? Bool ?? false {
@@ -235,23 +153,11 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
             completionHandler([])
         }
     }
+    
+}
 
-    fileprivate func dealWithRemoteNotification(_ userInfo: [AnyHashable: Any]?, fromLaunch: Bool = false) {
-        guard let userInfo = userInfo, let conversationId = userInfo["conversation_id"] as? String else {
-            return
-        }
-        
-        DispatchQueue.global().async {
-            guard let conversation = ConversationDAO.shared.getConversation(conversationId: conversationId), conversation.status == ConversationStatus.SUCCESS.rawValue else {
-                return
-            }
-            DispatchQueue.main.async {
-                UIApplication.homeNavigationController?.pushViewController(withBackRoot: ConversationViewController.instance(conversation: conversation))
-            }
-        }
-        UNUserNotificationCenter.current().removeAllNotifications()
-    }
-
+extension AppDelegate {
+    
     func handerQuickAction(_ response: UNNotificationResponse) -> Bool {
         let categoryIdentifier = response.notification.request.content.categoryIdentifier
         let actionIdentifier = response.actionIdentifier
@@ -259,7 +165,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         let userInfo = response.notification.request.content.userInfo
         return handerQuickAction(categoryIdentifier: categoryIdentifier, actionIdentifier: actionIdentifier, inputText: inputText, userInfo: userInfo)
     }
-
+    
     @discardableResult
     func handerQuickAction(categoryIdentifier: String, actionIdentifier: String, inputText: String?, userInfo: [AnyHashable : Any]) -> Bool {
         guard categoryIdentifier == NotificationCategoryIdentifier.message, AccountAPI.shared.didLogin else {
@@ -291,5 +197,109 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         }
         return true
     }
+    
 }
 
+extension AppDelegate {
+    
+    private func checkLogin() {
+        window.backgroundColor = .black
+        if AccountAPI.shared.didLogin {
+            window.rootViewController = makeInitialViewController()
+            if ContactsManager.shared.authorization == .authorized && CommonUserDefault.shared.isUploadContacts {
+                DispatchQueue.global().asyncAfter(deadline: .now() + 2, execute: {
+                    PhoneContactAPI.shared.upload(contacts: ContactsManager.shared.contacts)
+                })
+            }
+        } else {
+            if UIApplication.shared.isProtectedDataAvailable {
+                window.rootViewController = LoginNavigationController.instance()
+            } else {
+                window.rootViewController = R.storyboard.launchScreen().instantiateInitialViewController()
+            }
+        }
+        window.makeKeyAndVisible()
+    }
+    
+    private func configAnalytics() {
+        guard UIApplication.shared.isProtectedDataAvailable else {
+            return
+        }
+
+        if let account = AccountAPI.shared.account {
+            Bugsnag.configuration()?.setUser(account.user_id, withName: account.full_name, andEmail: account.identity_number)
+            Crashlytics.sharedInstance().setUserIdentifier(account.user_id)
+            Crashlytics.sharedInstance().setUserName(account.full_name)
+            Crashlytics.sharedInstance().setUserEmail(account.identity_number)
+            Crashlytics.sharedInstance().setObjectValue(Bundle.main.bundleIdentifier ?? "", forKey: "Package")
+        }
+
+        CommonUserDefault.shared.checkUpdateOrInstallVersion()
+        CommonUserDefault.shared.updateFirstLaunchDateIfNeeded()
+    }
+    
+    private func checkJailbreak() {
+        guard UIDevice.isJailbreak else {
+            return
+        }
+        Keychain.shared.clearPIN()
+    }
+    
+    private func initBugsnag() {
+        guard let apiKey = MixinKeys.bugsnag else {
+            return
+        }
+        Bugsnag.start(withApiKey: apiKey)
+    }
+    
+    private func checkServerData(isPushKit: Bool = false) {
+        guard AccountAPI.shared.didLogin else {
+            return
+        }
+        guard !DatabaseUserDefault.shared.hasUpgradeDatabase() else {
+            return
+        }
+        WebSocketService.shared.checkConnectStatus()
+
+        cancelBackgroundTask()
+        self.backgroundTaskID = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            self.cancelBackgroundTask()
+        })
+        let timeInterval: TimeInterval = !isPushKit || BlazeMessageDAO.shared.getCount() + JobDAO.shared.getCount() > 50 ? 120 : 20
+        self.backgroundTime = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: false) { (time) in
+            self.cancelBackgroundTask()
+        }
+    }
+    
+    private func cancelBackgroundTask() {
+        self.backgroundTime?.invalidate()
+        self.backgroundTime = nil
+        if backgroundTaskID != .invalid {
+            UIApplication.shared.endBackgroundTask(backgroundTaskID)
+            backgroundTaskID = .invalid
+        }
+    }
+    
+    private func updateSharedImageCacheConfig() {
+        SDImageCacheConfig.default.maxDiskSize = 1024 * bytesPerMegaByte
+        SDImageCacheConfig.default.maxDiskAge = -1
+        SDImageCacheConfig.default.diskCacheExpireType = .accessDate
+    }
+    
+    private func dealWithRemoteNotification(_ userInfo: [AnyHashable: Any]?, fromLaunch: Bool = false) {
+        guard let userInfo = userInfo, let conversationId = userInfo["conversation_id"] as? String else {
+            return
+        }
+        
+        DispatchQueue.global().async {
+            guard let conversation = ConversationDAO.shared.getConversation(conversationId: conversationId), conversation.status == ConversationStatus.SUCCESS.rawValue else {
+                return
+            }
+            DispatchQueue.main.async {
+                UIApplication.homeNavigationController?.pushViewController(withBackRoot: ConversationViewController.instance(conversation: conversation))
+            }
+        }
+        UNUserNotificationCenter.current().removeAllNotifications()
+    }
+    
+}

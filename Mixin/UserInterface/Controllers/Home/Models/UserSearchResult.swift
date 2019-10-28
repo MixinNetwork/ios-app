8da5ec85c1fe3447a48a6f38f8182af99b021f9f
wuyueyang <wuyueyang@mixin.one>
2019-10-28 21:39:04 +0800

    Update search

--- Mixin/UserInterface/Controllers/Home/Models/UserSearchResult.swift
+++ Mixin/UserInterface/Controllers/Home/Models/UserSearchResult.swift
@@ -4,20 +4,24 @@ class UserSearchResult: SearchResult {
     
     let user: UserItem
     
+    private let keyword: String
+    
     init(user: UserItem, keyword: String) {
         self.user = user
-        let title = SearchResult.attributedText(text: user.fullName,
-                                                 textAttributes: SearchResult.titleAttributes,
-                                                 keyword: keyword,
-                                                 keywordAttributes: SearchResult.highlightedTitleAttributes)
+        self.keyword = keyword
         let badgeImage = SearchResult.userBadgeImage(isVerified: user.isVerified,
-                                                      appId: user.appId)
-        let description = SearchResult.description(user: user, keyword: keyword)
+                                                     appId: user.appId)
         super.init(iconUrl: user.avatarUrl,
-                   title: title,
                    badgeImage: badgeImage,
-                   superscript: nil,
-                   description: description)
+                   superscript: nil)
+    }
+    
+    override func updateTitleAndDescription() {
+        title = SearchResult.attributedText(text: user.fullName,
+                                            textAttributes: SearchResult.titleAttributes,
+                                            keyword: keyword,
+                                            keywordAttributes: SearchResult.highlightedTitleAttributes)
+        description = SearchResult.description(user: user, keyword: keyword)
     }
     
 }

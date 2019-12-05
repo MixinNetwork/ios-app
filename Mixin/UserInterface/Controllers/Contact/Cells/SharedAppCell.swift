a513ce3d4d72232700c7350b9219a702ed1eb4cc
wuyueyang <wuyueyang@mixin.one>
2019-12-05 21:11:22 +0800

    Show 2 lines of biography

--- Mixin/UserInterface/Controllers/Contact/Cells/SharedAppCell.swift
+++ Mixin/UserInterface/Controllers/Contact/Cells/SharedAppCell.swift
@@ -4,4 +4,9 @@ class SharedAppCell: ModernSelectedBackgroundCell {
     
     @IBOutlet weak var infoView: PeerInfoView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        infoView.descriptionLabel.numberOfLines = 2
+    }
+    
 }

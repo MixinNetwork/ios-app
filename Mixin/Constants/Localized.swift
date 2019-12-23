32cd42ea7d40627ba6e8d2f72a7170ca9291a7c5
wuyueyang <wuyueyang@mixin.one>
2019-12-23 18:33:13 +0800

    Update localized

--- Mixin/Constants/Localized.swift
+++ Mixin/Constants/Localized.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+public func LocalizedString(_ key: String, comment: String) -> String {
+    let localText = NSLocalizedString(key, comment: "")
+    return localText == key ? comment :  localText
+}
+
 public class Localized {
 
     // Common
--- Mixin/Extension/LocalizedExtension.swift
+++ Mixin/Extension/LocalizedExtension.swift
@@ -1,10 +1,5 @@
 import UIKit
 
-public func LocalizedString(_ key: String, comment: String) -> String {
-    let localText = NSLocalizedString(key, comment: "")
-    return localText == key ? comment :  localText
-}
-
 extension UIButton {
 
     @IBInspectable

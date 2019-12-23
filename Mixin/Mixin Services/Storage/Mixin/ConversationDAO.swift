842110629220e981c3c02f07bd33c6c766757f1a
wuyueyang <wuyueyang@mixin.one>
2019-12-23 16:43:41 +0800

    Move search methods into main App

--- Mixin/Mixin Services/Storage/Mixin/ConversationDAO.swift
+++ Mixin/Mixin Services/Storage/Mixin/ConversationDAO.swift
@@ -50,20 +50,6 @@ public final class ConversationDAO {
         WHERE muteUntil < ?
     )
     """
-    private static let sqlSearchMessages = """
-    SELECT m.conversation_id, c.category,
-    CASE c.category WHEN 'CONTACT' THEN u.full_name ELSE c.name END,
-    CASE c.category WHEN 'CONTACT' THEN u.avatar_url ELSE c.icon_url END,
-    CASE c.category WHEN 'CONTACT' THEN u.user_id ELSE NULL END,
-    u.is_verified, u.app_id, COUNT(m.conversation_id)
-    FROM messages m
-    LEFT JOIN conversations c ON m.conversation_id = c.conversation_id
-    LEFT JOIN users u ON c.owner_id = u.user_id
-    WHERE m.category in ('SIGNAL_TEXT', 'SIGNAL_DATA','PLAIN_TEXT','PLAIN_DATA') AND m.status != 'FAILED'
-    AND (m.content LIKE ? ESCAPE '/' OR m.name LIKE ? ESCAPE '/')
-    GROUP BY m.conversation_id
-    ORDER BY c.pin_time DESC, c.last_message_created_at DESC
-    """
     
     func getUnreadMessageCount() -> Int {
         let value = MixinDatabase.shared.scalar(sql: ConversationDAO.sqlUnreadMessageCount, values: [Date().toUTCString()]).int64Value
@@ -173,58 +159,6 @@ public final class ConversationDAO {
         return MixinDatabase.shared.getCodables(sql: sql, values: [keyword, keyword])
     }
     
-    func getConversation(withMessageLike keyword: String, limit: Int?, callback: (CoreStatement) -> Void) -> [MessagesWithinConversationSearchResult] {
-        var sql = ConversationDAO.sqlSearchMessages
-        if let limit = limit {
-            sql += " LIMIT \(limit)"
-        }
-        let keyword = "%\(keyword.sqlEscaped)%"
-        let stmt = StatementSelectSQL(sql: sql)
-        var items = [MessagesWithinConversationSearchResult]()
-        let cs = try! MixinDatabase.shared.database.prepare(stmt)
-        callback(cs)
-        cs.bind(keyword, toIndex: 0)
-        cs.bind(keyword, toIndex: 1)
-        while (try? cs.step()) ?? false {
-            var i = -1
-            var autoIncrement: Int {
-                i += 1
-                return i
-            }
-            let conversationId: String = cs.value(atIndex: autoIncrement) ?? ""
-            let categoryString: String = cs.value(atIndex: autoIncrement) ?? ""
-            guard let category = ConversationCategory(rawValue: categoryString) else {
-                continue
-            }
-            let name = cs.value(atIndex: autoIncrement) ?? ""
-            let iconUrl = cs.value(atIndex: autoIncrement) ?? ""
-            let userId = cs.value(atIndex: autoIncrement) ?? ""
-            let userIsVerified = cs.value(atIndex: autoIncrement) ?? false
-            let userAppId: String? = cs.value(atIndex: autoIncrement)
-            let relatedMessageCount = cs.value(atIndex: autoIncrement) ?? 0
-            let item: MessagesWithinConversationSearchResult
-            switch category {
-            case .CONTACT:
-                item = MessagesWithUserSearchResult(conversationId: conversationId,
-                                                    name: name,
-                                                    iconUrl: iconUrl,
-                                                    userId: userId,
-                                                    userIsVerified: userIsVerified,
-                                                    userAppId: userAppId,
-                                                    relatedMessageCount: relatedMessageCount,
-                                                    keyword: keyword)
-            case .GROUP:
-                item = MessagesWithGroupSearchResult(conversationId: conversationId,
-                                                     name: name,
-                                                     iconUrl: iconUrl,
-                                                     relatedMessageCount: relatedMessageCount,
-                                                     keyword: keyword)
-            }
-            items.append(item)
-        }
-        return items
-    }
-
     func getOriginalConversation(conversationId: String) -> Conversation? {
         return MixinDatabase.shared.getCodable(condition: Conversation.Properties.conversationId == conversationId)
     }
--- Mixin/Mixin Services/Storage/Mixin/MessageDAO.swift
+++ Mixin/Mixin Services/Storage/Mixin/MessageDAO.swift
@@ -112,13 +112,6 @@ public final class MessageDAO {
         WHERE a.album_id = messages.album_id AND s.name = messages.name
     ) WHERE category LIKE '%_STICKER' AND ifnull(sticker_id, '') = ''
     """
-    static let sqlSearchMessageContent = """
-    SELECT m.id, m.category, m.content, m.created_at, u.user_id, u.full_name, u.avatar_url, u.is_verified, u.app_id
-        FROM messages m
-        LEFT JOIN users u ON m.user_id = u.user_id
-        WHERE conversation_id = ? AND m.category in ('SIGNAL_TEXT', 'SIGNAL_DATA','PLAIN_TEXT','PLAIN_DATA')
-        AND m.status != 'FAILED' AND (m.content LIKE ? ESCAPE '/' OR m.name LIKE ? ESCAPE '/')
-    """
     
     static let sqlQueryGalleryItem = """
     SELECT m.conversation_id, m.id, m.category, m.media_url, m.media_mime_type, m.media_width,
@@ -355,55 +348,6 @@ public final class MessageDAO {
     func getLastNMessages(conversationId: String, count: Int) -> [MessageItem] {
         let messages: [MessageItem] =  MixinDatabase.shared.getCodables(sql: MessageDAO.sqlQueryLastNMessages, values: [conversationId, count])
         return messages.reversed()
-    }
-    
-    func getMessages(conversationId: String, contentLike keyword: String, belowMessageId location: String?, limit: Int?) -> [MessageSearchResult] {
-        var results = [MessageSearchResult]()
-        
-        var sql: String!
-        if let location = location {
-            let rowId = MixinDatabase.shared.getRowId(tableName: Message.tableName,
-                                                      condition: Message.Properties.messageId == location)
-            sql = MessageDAO.sqlSearchMessageContent + " AND m.ROWID < \(rowId)"
-        } else {
-            sql = MessageDAO.sqlSearchMessageContent
-        }
-        if let limit = limit {
-            sql += " ORDER BY m.created_at DESC LIMIT \(limit)"
-        } else {
-            sql += " ORDER BY m.created_at DESC"
-        }
-        
-        do {
-            let stmt = StatementSelectSQL(sql: sql)
-            let cs = try MixinDatabase.shared.database.prepare(stmt)
-            
-            let bindingCounter = Counter(value: 0)
-            let wildcardedKeyword = "%\(keyword.sqlEscaped)%"
-            cs.bind(conversationId, toIndex: bindingCounter.advancedValue)
-            cs.bind(wildcardedKeyword, toIndex: bindingCounter.advancedValue)
-            cs.bind(wildcardedKeyword, toIndex: bindingCounter.advancedValue)
-            
-            while try cs.step() {
-                let counter = Counter(value: -1)
-                let result = MessageSearchResult(conversationId: conversationId,
-                                                 messageId: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 category: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 content: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 createdAt: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 userId: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 fullname: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 avatarUrl: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 isVerified: cs.value(atIndex: counter.advancedValue) ?? false,
-                                                 appId: cs.value(atIndex: counter.advancedValue) ?? "",
-                                                 keyword: keyword)
-                results.append(result)
-            }
-        } catch {
-            Reporter.report(error: error)
-        }
-        
-        return results
     }
     
     func getInvitationMessage(conversationId: String, inviteeUserId: String) -> Message? {

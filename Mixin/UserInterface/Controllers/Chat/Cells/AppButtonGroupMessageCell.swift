a7b41fe7de0152acab670d00a98b9a982262e46e
wuyueyang <wuyueyang@mixin.one>
2019-12-26 15:07:03 +0800

    Update app button group cell

--- Mixin/UserInterface/Controllers/Chat/Cells/AppButtonGroupMessageCell.swift
+++ Mixin/UserInterface/Controllers/Chat/Cells/AppButtonGroupMessageCell.swift
@@ -8,7 +8,7 @@ class AppButtonGroupMessageCell: DetailInfoMessageCell {
 
     weak var appButtonDelegate: AppButtonGroupMessageCellDelegate?
 
-    private var buttonViews = [UIView]()
+    private var buttons = [UIButton]()
     
     override var contentFrame: CGRect {
         return (viewModel as? AppButtonGroupViewModel)?.buttonGroupFrame ?? .zero
@@ -17,29 +17,29 @@ class AppButtonGroupMessageCell: DetailInfoMessageCell {
     override func render(viewModel: MessageViewModel) {
         super.render(viewModel: viewModel)
         if let viewModel = viewModel as? AppButtonGroupViewModel, let appButtons = viewModel.message.appButtons {
-            buttonViews.forEach {
+            buttons.forEach {
                 $0.removeFromSuperview()
             }
-            buttonViews = []
+            buttons = []
             for (i, frame) in viewModel.frames.enumerated() {
                 let buttonContent = appButtons[i]
-                let button = UIButton(frame: frame.button)
-                contentView.addSubview(button)
-                button.setBackgroundImage(R.image.ic_app_button_normal(), for: .normal)
-                button.setBackgroundImage(R.image.ic_app_button_selected(), for: .highlighted)
+                let button = UIButton(type: .system)
+                button.frame = frame
+                button.setTitle(buttonContent.label, for: .normal)
+                button.setTitleColor(UIColor(hexString: buttonContent.color) ?? .gray, for: .normal)
+                if let label = button.titleLabel {
+                    label.numberOfLines = 0
+                    label.font = MessageFontSet.appButtonTitle.scaled
+                    label.adjustsFontForContentSizeCategory = true
+                    label.lineBreakMode = .byCharWrapping
+                }
+                button.backgroundColor = .background
+                button.layer.cornerRadius = 8
+                button.clipsToBounds = true
                 button.tag = i
                 button.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
-                buttonViews.append(button)
-                let label = UILabel(frame: frame.label)
-                contentView.addSubview(label)
-                label.numberOfLines = 0
-                label.text = buttonContent.label
-                label.font = MessageFontSet.appButtonTitle.scaled
-                label.adjustsFontForContentSizeCategory = true
-                label.textColor = UIColor(hexString: buttonContent.color) ?? .gray
-                label.textAlignment = .left
-                label.lineBreakMode = .byCharWrapping
-                buttonViews.append(label)
+                buttons.append(button)
+                contentView.addSubview(button)
             }
         }
     }
--- Mixin/UserInterface/Controllers/Chat/Model/AppButtonGroupViewModel.swift
+++ Mixin/UserInterface/Controllers/Chat/Model/AppButtonGroupViewModel.swift
@@ -3,13 +3,12 @@ import UIKit
 class AppButtonGroupViewModel: DetailInfoMessageViewModel {
     
     private static let titleMargin = Margin(leading: 16, trailing: 16, top: 10, bottom: 12)
+    private static let buttonMargin = Margin(leading: 5, trailing: 5, top: 1, bottom: 3)
     
-    var frames = [(button: CGRect, label: CGRect)]()
+    var frames = [CGRect]()
     var buttonGroupFrame = CGRect.zero
     
-    private let margin = Margin(leading: 10, trailing: 10, top: 0, bottom: 0)
-    
-    private var buttonSizes = [CGSize]()
+    private let margin = Margin(leading: 12, trailing: 12, top: 0, bottom: 0)
     
     override init(message: MessageItem) {
         super.init(message: message)
@@ -19,44 +18,42 @@ class AppButtonGroupViewModel: DetailInfoMessageViewModel {
     override func layout(width: CGFloat, style: MessageViewModel.Style) {
         super.layout(width: width, style: style)
         layoutDetailInfo(backgroundImageFrame: backgroundImageFrame)
-        let boundingSize = CGSize(width: width - AppButtonGroupViewModel.titleMargin.horizontal - margin.horizontal,
+        
+        frames = []
+        buttonGroupFrame = .zero
+        
+        let boundingSize = CGSize(width: width - Self.buttonMargin.horizontal - Self.titleMargin.horizontal - margin.horizontal,
                                   height: UIView.layoutFittingExpandedSize.height)
         let titleAttributes = [NSAttributedString.Key.font: MessageFontSet.appButtonTitle.scaled]
-        buttonSizes = message.appButtons?.map({
+        let buttonSizes: [CGSize] = message.appButtons?.map({
             let titleSize = ($0.label as NSString).boundingRect(with: boundingSize,
                                                                 options: [.usesLineFragmentOrigin, .usesFontLeading],
                                                                 attributes: titleAttributes,
                                                                 context: nil)
-            return CGSize(width: ceil(titleSize.width + AppButtonGroupViewModel.titleMargin.horizontal),
-                          height: ceil(titleSize.height + AppButtonGroupViewModel.titleMargin.vertical))
+            return CGSize(width: ceil(titleSize.width + Self.titleMargin.horizontal),
+                          height: ceil(titleSize.height + Self.titleMargin.vertical))
         }) ?? []
+        
         var bottomRight = CGPoint(x: margin.leading, y: 0)
-        var y: CGFloat = 0
-        var frames = [CGRect]()
+        var y: CGFloat = style.contains(.fullname) ? fullnameFrame.height : 0
         for buttonSize in buttonSizes {
             let frame: CGRect
-            if bottomRight.x + buttonSize.width + margin.trailing <= width || abs(bottomRight.x - margin.leading) < 0.1 {
-                frame = CGRect(x: bottomRight.x, y: y, width: buttonSize.width, height: buttonSize.height)
+            let isFirstButtonOfTheLine = abs(bottomRight.x - margin.leading) < 1
+            if bottomRight.x + buttonSize.width + Self.buttonMargin.horizontal + margin.trailing <= width || isFirstButtonOfTheLine {
+                let leadingMargin = isFirstButtonOfTheLine ? Self.buttonMargin.leading : Self.buttonMargin.horizontal
+                frame = CGRect(x: bottomRight.x + leadingMargin,
+                               y: y + Self.buttonMargin.top,
+                               width: buttonSize.width,
+                               height: buttonSize.height)
             } else {
-                frame = CGRect(x: margin.leading, y: bottomRight.y, width: buttonSize.width, height: buttonSize.height)
-                y = frame.origin.y
+                frame = CGRect(x: margin.leading + Self.buttonMargin.leading,
+                               y: bottomRight.y + Self.buttonMargin.vertical,
+                               width: buttonSize.width,
+                               height: buttonSize.height)
+                y = bottomRight.y + Self.buttonMargin.bottom
             }
             frames.append(frame)
             bottomRight = CGPoint(x: frame.maxX, y: frame.maxY)
-        }
-        let buttonMargin = AppButtonGroupViewModel.titleMargin
-        self.frames = frames.map({
-            let labelFrame = CGRect(x: $0.origin.x + buttonMargin.leading,
-                                    y: $0.origin.y + buttonMargin.top,
-                                    width: $0.width - buttonMargin.horizontal,
-                                    height: $0.height - buttonMargin.vertical)
-            return (button: $0, label: labelFrame)
-        })
-        if style.contains(.fullname) {
-            self.frames = self.frames.map {
-                (button: $0.button.offsetBy(dx: 0, dy: fullnameFrame.height),
-                 label: $0.label.offsetBy(dx: 0, dy: fullnameFrame.height))
-            }
         }
         buttonGroupFrame = frames.reduce(.zero, { $0.union($1) })
         if let lastFrame = frames.last {

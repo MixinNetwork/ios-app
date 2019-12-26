7cca2e580922b36cc6f9f1ceedff5f73fb211672
wuyueyang <wuyueyang@mixin.one>
2019-12-26 13:31:33 +0800

    Fix corrupted components

--- Mixin/UserInterface/Controllers/Chat/UserHandleViewController.swift
+++ Mixin/UserInterface/Controllers/Chat/UserHandleViewController.swift
@@ -11,7 +11,7 @@ class UserHandleViewController: UITableViewController, ConversationAccessible {
     private lazy var tableHeaderView: UIView = {
         let frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 100)
         let view = UserHandleTableHeaderView(frame: frame)
-        view.backgroundColor = .background
+        view.backgroundColor = .clear
         view.clipsToBounds = true
         return view
     }()

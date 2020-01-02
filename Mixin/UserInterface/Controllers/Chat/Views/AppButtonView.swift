d5923fba465aa14b7ea1a6877f1b03d154c54cb4
wuyueyang <wuyueyang@mixin.one>
2020-01-02 12:12:05 +0800

    Fix app button becomes transparent when highlighted under iOS 12

--- Mixin/UserInterface/Controllers/Chat/Views/AppButtonView.swift
+++ Mixin/UserInterface/Controllers/Chat/Views/AppButtonView.swift
@@ -5,7 +5,7 @@ class AppButtonView: UIView {
     static let buttonMargin = MessageViewModel.Margin(leading: 5, trailing: 5, top: 1, bottom: 3)
     static let titleMargin = MessageViewModel.Margin(leading: 16, trailing: 16, top: 10, bottom: 12)
     
-    let button = UIButton(type: .system)
+    let button = AppButton()
     
     private let cornerRadius: CGFloat = 8
     

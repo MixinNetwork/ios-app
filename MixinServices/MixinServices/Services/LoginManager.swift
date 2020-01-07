ed159bb3f00514143ccc95c0af45f4a4b400bc22
Jun Tang <tangjun@shou.tv>
2020-01-07 20:03:11 +0800

    Remove darwin notification

--- MixinServices/MixinServices/Services/LoginManager.swift
+++ MixinServices/MixinServices/Services/LoginManager.swift
@@ -8,32 +8,21 @@ public var myIdentityNumber: String {
     LoginManager.shared.account?.identity_number ?? "00000"
 }
 
-fileprivate let accountDidChangeDarwinNotificationName = CFNotificationName(rawValue: "one.mixin.services.darwin.account.did.change" as CFString)
-
 public final class LoginManager {
     
     public static let shared = LoginManager()
     public static let accountDidChangeNotification = Notification.Name("one.mixin.services.account.did.change")
     public static let didLogoutNotification = Notification.Name("one.mixin.services.did.logout")
-    
-    private let darwinNotifyCenter = CFNotificationCenterGetDarwinNotifyCenter()
-    
+
     fileprivate var _account: Account?
+    fileprivate var _isLoggedIn = false
     fileprivate var lock = pthread_rwlock_t()
-    fileprivate var ignoreNextDarwinNotification = false
-    
-    private var selfAsOpaquePointer: UnsafeMutableRawPointer {
-        Unmanaged.passUnretained(self).toOpaque()
-    }
-    
+
     public var isLoggedIn: Bool {
-        guard account != nil else {
-            return false
-        }
-        guard let token = AppGroupUserDefaults.Account.sessionSecret, !token.isEmpty else {
-            return false
-        }
-        return true
+        pthread_rwlock_rdlock(&lock)
+        let loggedIn = _isLoggedIn
+        pthread_rwlock_unlock(&lock)
+        return loggedIn
     }
     
     public var account: Account? {
@@ -46,25 +35,23 @@ public final class LoginManager {
     private init() {
         pthread_rwlock_init(&lock, nil)
         _account = LoginManager.getAccountFromUserDefaults()
-        CFNotificationCenterAddObserver(darwinNotifyCenter, selfAsOpaquePointer, notificationCallback, accountDidChangeDarwinNotificationName.rawValue, nil, .deliverImmediately)
-    }
-    
-    deinit {
-        CFNotificationCenterRemoveEveryObserver(darwinNotifyCenter, selfAsOpaquePointer)
+        _isLoggedIn = _account != nil && !(AppGroupUserDefaults.Account.sessionSecret?.isEmpty ?? true)
     }
     
     fileprivate static func getAccountFromUserDefaults() -> Account? {
-        if let data = AppGroupUserDefaults.Account.serializedAccount {
-            return try? JSONDecoder.default.decode(Account.self, from: data)
-        } else {
+        guard let data = AppGroupUserDefaults.Account.serializedAccount else {
             return nil
         }
+
+        return try? JSONDecoder.default.decode(Account.self, from: data)
     }
     
     public func setAccount(_ account: Account?, updateUserTable: Bool = true) {
         pthread_rwlock_wrlock(&lock)
         _account = account
+        _isLoggedIn = account != nil && !(AppGroupUserDefaults.Account.sessionSecret?.isEmpty ?? true)
         pthread_rwlock_unlock(&lock)
+
         if let account = account {
             if let data = try? JSONEncoder.default.encode(account) {
                 AppGroupUserDefaults.Account.serializedAccount = data
@@ -78,8 +65,6 @@ public final class LoginManager {
         } else {
             AppGroupUserDefaults.Account.serializedAccount = nil
         }
-        ignoreNextDarwinNotification = true
-        CFNotificationCenterPostNotification(darwinNotifyCenter, accountDidChangeDarwinNotificationName, nil, nil, true)
     }
     
     public func logout(from: String) {
@@ -98,19 +83,4 @@ public final class LoginManager {
         }
     }
     
-}
-
-fileprivate func notificationCallback(center: CFNotificationCenter?, observer: UnsafeMutableRawPointer?, name: CFNotificationName?, object: UnsafeRawPointer?, userInfo: CFDictionary?) {
-    guard let observer = observer else {
-        return
-    }
-    let manager = Unmanaged<LoginManager>.fromOpaque(observer).takeUnretainedValue()
-    guard !manager.ignoreNextDarwinNotification else {
-        manager.ignoreNextDarwinNotification = false
-        return
-    }
-    let account = LoginManager.getAccountFromUserDefaults()
-    pthread_rwlock_wrlock(&manager.lock)
-    manager._account = account
-    pthread_rwlock_unlock(&manager.lock)
 }

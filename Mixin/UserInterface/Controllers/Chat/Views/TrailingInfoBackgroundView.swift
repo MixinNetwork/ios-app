f01a9bf12fc911ca40899be0e9d4a74a86aabc1b
wuyueyang <wuyueyang@mixin.one>
2020-02-25 14:58:54 +0800

    Update video tag's background

--- Mixin/UserInterface/Controllers/Chat/Views/TrailingInfoBackgroundView.swift
+++ Mixin/UserInterface/Controllers/Chat/Views/TrailingInfoBackgroundView.swift
@@ -6,7 +6,7 @@ class TrailingInfoBackgroundView: UIView {
     
     convenience init() {
         self.init(frame: CGRect(x: 0, y: 0, width: 60, height: Self.height))
-        backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        backgroundColor = .tagBackground
         clipsToBounds = true
         layer.cornerRadius = 9
     }

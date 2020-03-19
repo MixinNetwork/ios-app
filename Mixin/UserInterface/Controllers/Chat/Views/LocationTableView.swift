30bde44a5adc14057b183446776b8e112189b364
wuyueyang <wuyueyang@mixin.one>
2020-03-19 13:21:03 +0800

    Fix bottom table becomes irresponsible

--- Mixin/UserInterface/Controllers/Chat/Views/LocationTableView.swift
+++ Mixin/UserInterface/Controllers/Chat/Views/LocationTableView.swift
@@ -2,15 +2,28 @@ import UIKit
 
 class LocationTableWrapperView: UIView {
     
+    // If this view is on screen during userInterfaceStyle changing, e.g. light -> dark,
+    // UIView.mask becomes a user-interaction blocking subview for some mysterious reason.
+    // This is unrecoverable in whole app lifecycle.
+    // Will be using hit test to bypass the mask view and return the tableView.
+    private weak var tableView: UITableView?
+    
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let superHitTest = super.hitTest(point, with: event)
         guard let mask = mask else {
             return superHitTest
         }
         if mask.frame.contains(point) {
-            return superHitTest
+            return tableView ?? superHitTest
         } else {
             return nil
+        }
+    }
+    
+    override func addSubview(_ view: UIView) {
+        super.addSubview(view)
+        if let view = view as? UITableView {
+            self.tableView = view
         }
     }
     

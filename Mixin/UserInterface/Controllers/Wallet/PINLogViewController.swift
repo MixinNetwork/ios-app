0dd4220e7e43b8b546aa19f6bf40db5cd6f734b2
wuyueyang <wuyueyang@mixin.one>
2020-04-27 19:48:39 +0800

    Update PIN log

--- Mixin/UserInterface/Controllers/Wallet/PINLogViewController.swift
+++ Mixin/UserInterface/Controllers/Wallet/PINLogViewController.swift
@@ -1,70 +1,67 @@
 import UIKit
 import MixinServices
 
-class PINLogViewController: UITableViewController {
-
+class PINLogViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: ActivityIndicatorView!
+    
+    @IBOutlet weak var showActivityIndicatorConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hideActivityIndicatorConstraint: NSLayoutConstraint!
+    
     private let loadNextPageThreshold = 20
     private var logs = [PINLogResponse]()
     private var isLoading = false
     private var isPageEnded = false
-
+    
+    class func instance() -> UIViewController {
+        let vc = R.storyboard.wallet.pin_logs()!
+        let container = ContainerViewController.instance(viewController: vc, title: R.string.localizable.wallet_pin_logs())
+        return container
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(R.nib.pinLogCell)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
         fetchLogs()
     }
-
+    
     private func fetchLogs(offset: String? = nil) {
         guard !isLoading else {
             return
         }
         isLoading = true
         AccountAPI.shared.pinLogs(offset: logs.last?.createdAt) { [weak self](result) in
-            guard let weakSelf = self else {
+            guard let self = self else {
                 return
             }
-            weakSelf.isLoading = false
+            self.isLoading = false
             switch result{
             case let .success(logs):
-                if logs.count < weakSelf.loadNextPageThreshold {
-                    weakSelf.isPageEnded = true
+                if logs.count < self.loadNextPageThreshold {
+                    self.isPageEnded = true
                 }
-                weakSelf.logs += logs
-                weakSelf.tableView.reloadData()
-                weakSelf.tableView.checkEmpty(dataCount: weakSelf.logs.count, text: R.string.localizable.wallet_pin_logs_empty(), photo: R.image.wallet.ic_no_transaction()!)
+                self.logs += logs
+                self.tableView.reloadData()
+                self.tableView.checkEmpty(dataCount: self.logs.count,
+                                          text: R.string.localizable.wallet_pin_logs_empty(),
+                                          photo: R.image.wallet.ic_no_transaction()!)
+                self.view.layoutIfNeeded()
             case let .failure(error):
                 showAutoHiddenHud(style: .error, text: error.localizedDescription)
             }
+            if self.activityIndicator.isAnimating {
+                self.activityIndicator.stopAnimating()
+                self.hideActivityIndicatorConstraint.priority = .defaultHigh
+                UIView.animate(withDuration: 0.3) {
+                    self.view.layoutIfNeeded()
+                }
+            }
         }
     }
-
-    class func instance() -> UIViewController {
-        let vc = R.storyboard.wallet.pin_logs()!
-        let container = ContainerViewController.instance(viewController: vc, title: R.string.localizable.wallet_pin_logs())
-        return container
-    }
-
-}
-
-extension PINLogViewController {
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return logs.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.pin_logs, for: indexPath)!
-        let log = logs[indexPath.row]
-        cell.titleLabel.text = getDescription(by: log.code)
-        cell.ipLabel.text = log.ipAddress
-        cell.timeLabel.text = log.createdAt.toUTCDate().logDatetime()
-        return cell
-    }
-
+    
     private func getDescription(by code: String) -> String {
         switch code {
         case "VERIFICATION":
@@ -97,15 +94,41 @@ extension PINLogViewController {
             return code
         }
     }
+    
+}
 
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+extension PINLogViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return logs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.pin_logs, for: indexPath)!
+        let log = logs[indexPath.row]
+        cell.titleLabel.text = getDescription(by: log.code)
+        cell.ipLabel.text = log.ipAddress
+        cell.timeLabel.text = log.createdAt.toUTCDate().logDatetime()
+        return cell
+    }
+    
+}
+
+extension PINLogViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         guard logs.count - indexPath.row < loadNextPageThreshold, !isPageEnded else {
             return
         }
         fetchLogs()
     }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
 }

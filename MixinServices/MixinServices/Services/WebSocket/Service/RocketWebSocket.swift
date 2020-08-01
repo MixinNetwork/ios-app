fd03d8902de51e3f6c0010b8125faa3b75b9ad88
wuyueyang <wuyueyang@mixin.one>
2020-08-01 21:45:33 +0800

    Record WebSocket error log

--- MixinServices/MixinServices/Services/WebSocket/Service/RocketWebSocket.swift
+++ MixinServices/MixinServices/Services/WebSocket/Service/RocketWebSocket.swift
@@ -67,14 +67,18 @@ extension RocketWebSocket: SRWebSocketDelegate {
         }
         let nsError = error as NSError
         if nsError.domain == "com.squareup.SocketRocket" && nsError.code == 504 {
+            let log = "[RocketWebSocket][DidFailWithError]...websocket connect timeout ...\(nsError)"
             #if DEBUG
-            print("[RocketWebSocket][DidFailWithError]...websocket connect timeout ...\(nsError)")
+            NSLog(log)
             #endif
+            Logger.write(log: log)
             delegate?.websocketDidDisconnect(socket: self, isSwitchNetwork: true)
         } else {
+            let log = "[RocketWebSocket][DidFailWithError]\(err)"
             #if DEBUG
-            print("[RocketWebSocket][DidFailWithError]\(err)")
+            NSLog(log)
             #endif
+            Logger.write(log: log)
             delegate?.websocketDidDisconnect(socket: self, isSwitchNetwork: false)
 
             if nsError.domain == NSPOSIXErrorDomain && nsError.code == 57 {
@@ -118,9 +122,12 @@ extension RocketWebSocket: SRWebSocketDelegate {
             break
         }
 
+        let log = "[RocketWebSocket][\(errType)][\(code)]...wasClean:\(wasClean)...\(reason ?? "")"
         #if DEBUG
-        print("[RocketWebSocket][\(errType)][\(code)]...wasClean:\(wasClean)...\(reason ?? "")")
+        NSLog(log)
         #endif
+        Logger.write(log: log)
+        
         reporter.report(error: MixinServicesError.websocketError(errType: errType, errMessage: errMessage, errCode: code))
         delegate?.websocketDidDisconnect(socket: self, isSwitchNetwork: false)
     }

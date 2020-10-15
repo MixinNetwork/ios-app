a1c26c619c80e93440bc28078d5a7776673f5436
wuyueyang <wuyueyang@mixin.one>
2020-10-16 04:02:18 +0800

    Add ScreenHeightCompatibleLayoutConstraint

---
+++ Mixin/UserInterface/Models/ScreenHeight.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+enum ScreenHeight {
+    
+    /*
+     Extra short
+     iPhone 3.5”:                       320 x 480 pt
+     
+     Short
+     iPhone 4”:                         320 x 568 pt
+     
+     Medium
+     iPhone 4.7”:                       375 x 667 pt
+     iPhone 5.4” & 5.8” (Zoom mode):    320 x 693 pt
+     
+     Long
+     iPhone 5.5”:                       414 x 736 pt
+     iPhone 5.4” & 5.8”:                375 x 812 pt
+     
+     Extra Long
+     iPhone 6.1" (2020):                390 x 844 pt
+     iPhone 6.1” (2018–2019) & 6.5”:    414 x 896 pt
+     iPhone 6.7”:                       428 x 926 pt
+    */
+    
+    case extraShort
+    case short
+    case medium
+    case long
+    case extraLong
+    
+    static let current: ScreenHeight = {
+        let height = max(UIScreen.main.bounds.height, UIScreen.main.bounds.width)
+        if height <= 480 {
+            return .extraShort
+        } else if height <= (568 + 667) / 2 {
+            return .short
+        } else if height <= (693 + 736) / 2 {
+            return .medium
+        } else if height <= (812 + 844) / 2 {
+            return .long
+        } else {
+            return .extraLong
+        }
+    }()
+    
+}

2030175886aee047c545e60ae481154a99f9d7a8
wuyueyang <wuyueyang@mixin.one>
2020-10-16 15:55:38 +0800

    Update compatible constraint based on horizontal width

--- Mixin/UserInterface/Controllers/Wallet/Views/DepositFieldView.swift
+++ Mixin/UserInterface/Controllers/Wallet/Views/DepositFieldView.swift
@@ -13,16 +13,22 @@ class DepositFieldView: UIView, XibDesignable {
     @IBOutlet weak var assetIconView: AssetIconView!
     @IBOutlet weak var shadowView: SeparatorShadowView!
     
+    @IBOutlet weak var contentLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentSpacingConstraint: NSLayoutConstraint!
+    
     weak var delegate: DepositFieldViewDelegate?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         loadXib()
+        updateScreenSizeBasedConstraints()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadXib()
+        updateScreenSizeBasedConstraints()
     }
     
     @IBAction func copyAction(_ sender: Any) {
@@ -32,6 +38,15 @@ class DepositFieldView: UIView, XibDesignable {
     
     @IBAction func showQRCodeAction(_ sender: Any) {
         delegate?.depositFieldViewDidSelectShowQRCode(self)
+    }
+    
+    private func updateScreenSizeBasedConstraints() {
+        guard ScreenWidth.current <= .short else {
+            return
+        }
+        contentLeadingConstraint.constant = 20
+        contentTrailingConstraint.constant = 20
+        contentSpacingConstraint.constant = 16
     }
     
 }

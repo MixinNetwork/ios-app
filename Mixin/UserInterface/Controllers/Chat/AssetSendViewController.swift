1a8709ace7e5f70927a03d0c1f1ac31eb6e48444
wuyueyang <wuyueyang@mixin.one>
2020-11-13 11:58:19 +0800

    Sanitize image in AssetSendViewController

--- Mixin/UserInterface/Controllers/Chat/AssetSendViewController.swift
+++ Mixin/UserInterface/Controllers/Chat/AssetSendViewController.swift
@@ -280,14 +280,23 @@ class AssetSendViewController: UIViewController, MixinNavigationAnimating {
             } else {
                 let filename = "\(message.messageId).\(ExtensionName.jpeg)"
                 let targetUrl = AttachmentContainer.url(for: .photos, filename: filename)
-                if let targetPhoto = ImageUploadSanitizer.sanitizedImage(from: image).image, targetPhoto.saveToFile(path: targetUrl), FileManager.default.fileSize(targetUrl.path) > 0 {
-                    message.thumbImage = targetPhoto.base64Thumbnail()
+                switch ImageUploadSanitizer.sanitizedImage(from: image) {
+                case let (.some(image), .some(data)):
+                    do {
+                        try data.write(to: targetUrl)
+                        if FileManager.default.fileSize(targetUrl.path) <= 0 {
+                            fallthrough
+                        }
+                    } catch {
+                        fallthrough
+                    }
+                    message.thumbImage = image.base64Thumbnail()
                     message.mediaSize = FileManager.default.fileSize(targetUrl.path)
-                    message.mediaWidth = Int(targetPhoto.size.width)
-                    message.mediaHeight = Int(targetPhoto.size.height)
+                    message.mediaWidth = Int(image.size.width)
+                    message.mediaHeight = Int(image.size.height)
                     message.mediaMimeType = "image/jpeg"
                     message.mediaUrl = filename
-                } else {
+                default:
                     DispatchQueue.main.async {
                         self?.sendButton.isBusy = false
                         self?.alert(Localized.CHAT_SEND_PHOTO_FAILED)

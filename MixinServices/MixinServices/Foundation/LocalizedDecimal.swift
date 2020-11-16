a9af6c44bd0e77f9c303f4149c0f1f7d90e65f58
wuyueyang <wuyueyang@mixin.one>
2020-11-17 00:44:20 +0800

    Format decimal number string

---
+++ MixinServices/MixinServices/Foundation/LocalizedDecimal.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct LocalizedDecimal {
+    
+    private static let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.locale = .current
+        formatter.generatesDecimalNumbers = true
+        return formatter
+    }()
+    
+    public let generic: GenericDecimal
+    
+    public var doubleValue: Double {
+        generic.doubleValue
+    }
+    
+    public init?(string: String) {
+        let whitespaceFiltered = string.filter { !$0.isWhitespace }
+        guard let decimal = Self.formatter.number(from: whitespaceFiltered) as? Decimal else {
+            return nil
+        }
+        self.generic = GenericDecimal(decimal: decimal)
+    }
+    
+    public static func isValidDecimal(_ string: String) -> Bool {
+        if let _ = Self(string: string) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}

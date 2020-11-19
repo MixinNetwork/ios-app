04a94465273e3e8ffdade05e3f83fe1261baaa81
wuyueyang <wuyueyang@mixin.one>
2020-11-19 19:11:52 +0800

    Compress article images with max quality factor

--- MixinServices/MixinServices/Foundation/ImageUploadSanitizer.swift
+++ MixinServices/MixinServices/Foundation/ImageUploadSanitizer.swift
@@ -5,18 +5,19 @@ public enum ImageUploadSanitizer {
     public static let maxSize = CGSize(width: 1920, height: 1920)
     
     public static func sanitizedImage(from rawImage: UIImage) -> (image: UIImage?, data: Data?) {
-        guard let rawData = rawImage.jpegData(compressionQuality: JPEGCompressionQuality.high) else {
-            return (nil, nil)
-        }
         let isRawImageUndersized = min(rawImage.size.width, rawImage.size.height) <= min(maxSize.width, maxSize.height)
             && max(rawImage.size.width, rawImage.size.height) <= max(maxSize.width, maxSize.height)
-        if imageWithRatioMaybeAnArticle(rawImage.size) || isRawImageUndersized {
-            return (rawImage, rawData)
+        if imageWithRatioMaybeAnArticle(rawImage.size) {
+            let data = rawImage.jpegData(compressionQuality: JPEGCompressionQuality.max)
+            return (rawImage, data)
+        } else if isRawImageUndersized {
+            let data = rawImage.jpegData(compressionQuality: JPEGCompressionQuality.high)
+            return (rawImage, data)
         } else {
             let newSize = rawImage.size.sizeThatFits(maxSize)
             let newImage = rawImage.imageByScaling(to: newSize)
-            let newData = newImage?.jpegData(compressionQuality: JPEGCompressionQuality.high)
-            return (newImage, newData)
+            let data = newImage?.jpegData(compressionQuality: JPEGCompressionQuality.high)
+            return (newImage, data)
         }
     }
     

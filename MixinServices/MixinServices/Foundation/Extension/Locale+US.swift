6f2318a055806c9e134e7a9ee8c3a26c5d8eb480
wuyueyang <wuyueyang@mixin.one>
2020-11-22 21:37:11 +0800

    Update preferences with decimal

--- MixinServices/MixinServices/Foundation/Extension/Locale+US.swift
+++ MixinServices/MixinServices/Foundation/Extension/Locale+US.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 public extension Locale {
+    
     public static let us = Locale(identifier: "US")
+    public static let preferred: Locale = {
+        if let id = Locale.preferredLanguages.first {
+            return Locale(identifier: id)
+        } else {
+            return .current
+        }
+    }()
+    
 }
--- MixinServices/MixinServices/Services/API/Request/UserRequest.swift
+++ MixinServices/MixinServices/Services/API/Request/UserRequest.swift
@@ -9,12 +9,10 @@ public struct UserPreferenceRequest: Codable {
     public let accept_conversation_source: String?
     public let accept_search_source: String?
     public let fiat_currency: String?
+    public let transfer_notification_threshold: Decimal?
+    public let transfer_confirmation_threshold: Decimal?
     
-    // XXX: Does these two vars below losing precision?
-    public let transfer_notification_threshold: Double?
-    public let transfer_confirmation_threshold: Double?
-    
-    public init(full_name: String? = nil, avatar_base64: String? = nil, notification_token: String? = nil, receive_message_source: String? = nil, accept_conversation_source: String? = nil, fiat_currency: String? = nil, transfer_notification_threshold: Double? = nil, transfer_confirmation_threshold: Double? = nil, accept_search_source: String? = nil) {
+    public init(full_name: String? = nil, avatar_base64: String? = nil, notification_token: String? = nil, receive_message_source: String? = nil, accept_conversation_source: String? = nil, fiat_currency: String? = nil, transfer_notification_threshold: Decimal? = nil, transfer_confirmation_threshold: Decimal? = nil, accept_search_source: String? = nil) {
         self.full_name = full_name
         self.avatar_base64 = avatar_base64
         self.notification_token = notification_token

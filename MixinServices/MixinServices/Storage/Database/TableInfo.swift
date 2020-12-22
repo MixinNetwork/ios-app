f918f2aa8bad63374ad3b9349a420549d7f2ff81
wuyueyang <wuyueyang@mixin.one>
2020-12-22 12:27:11 +0800

    Auto synthesize CodingKeys for internal structs

--- MixinServices/MixinServices/Storage/Database/TableInfo.swift
+++ MixinServices/MixinServices/Storage/Database/TableInfo.swift
@@ -1,19 +1,11 @@
 import Foundation
 import GRDB
 
-internal struct TableInfo {
+internal struct TableInfo: Decodable, MixinFetchableRecord {
     
     // There're more columns in table_info, but those
     // are omitted since we don't need them currently
     
     let name: String
-    
-}
-
-extension TableInfo: Decodable, MixinFetchableRecord {
-    
-    internal enum CodingKeys: String, CodingKey {
-        case name
-    }
     
 }

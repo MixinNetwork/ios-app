eef7ef9034b9a1677a983ed3b748036500a77642
wuyueyang <wuyueyang@mixin.one>
2020-12-29 21:47:16 +0800

    Create table only if not exists

--- MixinServices/MixinServices/Database/Signal/SignalDatabase.swift
+++ MixinServices/MixinServices/Database/Signal/SignalDatabase.swift
@@ -14,17 +14,17 @@ public final class SignalDatabase: Database {
         var migrator = DatabaseMigrator()
         
         migrator.registerMigration("create_table") { db in
-            try db.execute(sql: "CREATE TABLE identities(id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT, registrationId INTEGER, publicKey BLOB, privateKey BLOB, nextPreKeyId INTEGER, timestamp REAL)")
-            try db.execute(sql: "CREATE TABLE prekeys(id INTEGER PRIMARY KEY AUTOINCREMENT, preKeyId INTEGER, record BLOB)")
-            try db.execute(sql: "CREATE TABLE ratchet_sender_keys(groupId TEXT, senderId TEXT, status TEXT, CONSTRAINT _multi_primary PRIMARY KEY(groupId, senderId))")
-            try db.execute(sql: "CREATE TABLE sender_keys(groupId TEXT, senderId TEXT, record BLOB, CONSTRAINT _multi_primary PRIMARY KEY(groupId, senderId))")
-            try db.execute(sql: "CREATE TABLE sessions(id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT, device INTEGER, record BLOB, timestamp REAL)")
-            try db.execute(sql: "CREATE TABLE signed_prekeys(id INTEGER PRIMARY KEY AUTOINCREMENT, preKeyId INTEGER, record BLOB, timestamp REAL)")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS identities(id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT, registrationId INTEGER, publicKey BLOB, privateKey BLOB, nextPreKeyId INTEGER, timestamp REAL)")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS prekeys(id INTEGER PRIMARY KEY AUTOINCREMENT, preKeyId INTEGER, record BLOB)")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS ratchet_sender_keys(groupId TEXT, senderId TEXT, status TEXT, CONSTRAINT _multi_primary PRIMARY KEY(groupId, senderId))")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS sender_keys(groupId TEXT, senderId TEXT, record BLOB, CONSTRAINT _multi_primary PRIMARY KEY(groupId, senderId))")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS sessions(id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT, device INTEGER, record BLOB, timestamp REAL)")
+            try db.execute(sql: "CREATE TABLE IF NOT EXISTS signed_prekeys(id INTEGER PRIMARY KEY AUTOINCREMENT, preKeyId INTEGER, record BLOB, timestamp REAL)")
             
-            try db.execute(sql: "CREATE UNIQUE INDEX identities_index_id ON identities(address)")
-            try db.execute(sql: "CREATE UNIQUE INDEX prekeys_index_id ON prekeys(preKeyId)")
-            try db.execute(sql: "CREATE UNIQUE INDEX sessions_multi_index ON sessions(address, device)")
-            try db.execute(sql: "CREATE UNIQUE INDEX signed_prekeys_index_id ON signed_prekeys(preKeyId)")
+            try db.execute(sql: "CREATE UNIQUE INDEX IF NOT EXISTS identities_index_id ON identities(address)")
+            try db.execute(sql: "CREATE UNIQUE INDEX IF NOT EXISTS prekeys_index_id ON prekeys(preKeyId)")
+            try db.execute(sql: "CREATE UNIQUE INDEX IF NOT EXISTS sessions_multi_index ON sessions(address, device)")
+            try db.execute(sql: "CREATE UNIQUE INDEX IF NOT EXISTS signed_prekeys_index_id ON signed_prekeys(preKeyId)")
         }
         
         return migrator

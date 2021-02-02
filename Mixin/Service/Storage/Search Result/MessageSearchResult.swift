bf9b55d9faf5d95e308da612a72af642c16b7c3f
wuyueyang <wuyueyang@mixin.one>
2021-02-02 17:51:16 +0800

    Fix wrong title when searching files

--- Mixin/Service/Storage/Search Result/MessageSearchResult.swift
+++ Mixin/Service/Storage/Search Result/MessageSearchResult.swift
@@ -37,7 +37,7 @@ class MessageSearchResult: SearchResult {
                                             keywordAttributes: SearchResult.highlightedTitleAttributes)
         
         if isData {
-            description = NSAttributedString(string: R.string.localizable.notification_content_live(),
+            description = NSAttributedString(string: R.string.localizable.notification_content_file(),
                                              attributes: SearchResult.normalDescriptionAttributes)
         } else {
             // TODO: Tokenize

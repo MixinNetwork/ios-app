784411124441f5b09cb5163ebfd107295e2058aa
wuyueyang <wuyueyang@mixin.one>
2021-05-27 17:58:13 +0800

    Test UUID token conversion

--- MixinServices/MixinServicesTests/Database/UserTests.swift
+++ MixinServices/MixinServicesTests/Database/UserTests.swift
@@ -8,16 +8,38 @@ class UserTests: XCTestCase {
         .first!
         .appendingPathComponent("mixin.db", isDirectory: false)
     
+    var db: UserDatabase!
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
         print("Testing with URL: \(url)")
-        let db = try SignalDatabase(url: url)
-        SignalDatabase.reloadCurrent(with: db)
+        db = try UserDatabase(url: url)
     }
     
     override func tearDownWithError() throws {
-        UserDatabase.closeCurrent()
+        autoreleasepool {
+            db = nil
+        }
         try FileManager.default.removeItem(at: url)
+    }
+    
+    func testUUIDTokenizing() {
+        let uuids = [
+            UUID().uuidString,
+            UUID().uuidString,
+            UUID().uuidString,
+            UUID().uuidString,
+            UUID().uuidString,
+        ]
+        let uppercasedUUIDs = uuids.map { $0.uppercased() }
+        let lowercasedUUIDs = uuids.map { $0.lowercased() }
+        let uppercasedTokens = uppercasedUUIDs.map(uuidTokenString(uuidString:))
+        let lowercasedTokens = lowercasedUUIDs.map(uuidTokenString(uuidString:))
+        XCTAssertEqual(uppercasedUUIDs, uppercasedTokens.map(uuidString(uuidTokenString:)))
+        XCTAssertEqual(lowercasedUUIDs, lowercasedTokens.map(uuidString(uuidTokenString:)))
+        
+        let invalidUUID = "828514A6-7EEF-4545-BF18-8E136E7F11-5"
+        XCTAssertEqual(invalidUUID, uuidString(uuidTokenString: uuidTokenString(uuidString: invalidUUID)))
     }
     
 }

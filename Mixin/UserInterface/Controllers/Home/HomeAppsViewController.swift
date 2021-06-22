7b2d3ef629848a8c9137c1898c4cac94b3a3a6e1
fanyu <fanyukobe@gmail.com>
2021-06-22 17:59:35 +0800

    Change home-apps scroll direction to horizontal

--- Mixin/UserInterface/Controllers/Home/HomeAppsViewController.swift
+++ Mixin/UserInterface/Controllers/Home/HomeAppsViewController.swift
@@ -1,32 +1,25 @@
 import UIKit
 import MixinServices
-import AlignedCollectionViewFlowLayout
 
-final class HomeAppsViewController: ResizablePopupViewController {
+final class HomeAppsViewController: UIViewController {
     
     @IBOutlet weak var noPinnedHintLabel: UILabel!
     @IBOutlet weak var pinnedCollectionView: UICollectionView!
     @IBOutlet weak var pinnedCollectionLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var candidateCollectionView: UICollectionView!
-    @IBOutlet weak var candidateCollectionLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var pageControl: UIPageControl!
-
+    
     @IBOutlet weak var titleBarHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var pinnedCollectionViewLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var pinnedCollectionViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var pinnedWrapperHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var candidateCollectionViewHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var pageControlBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pageControlTopConstraint: NSLayoutConstraint!
     
-    override var resizableScrollView: UIScrollView? {
-        candidateCollectionView
-    }
+    private let cellCount = (perRow: 4, perColumn: ScreenHeight.current == .medium ? 3 : 4)
+    private let candidateCollectionCellSize = CGSize(width: 80, height: 100)
+    private var candidateCollectionLayout: HomeAppsFlowLayout!
     
-    private let cellCountPerRow = 4
-    private let cellSize = CGSize(width: 80, height: 100)
-    private let cellCountPerPage = (row: 4, column: ScreenHeight.current == .medium ? 3 : 4)
-    
-    private lazy var resizeGestureCoordinator = HomeAppResizeGestureCoordinator(scrollView: candidateCollectionView)
     private lazy var candidateEmptyHintLabel: UILabel = {
         let label = UILabel()
         label.text = R.string.localizable.home_apps_candidate_empty()
@@ -55,9 +48,7 @@ final class HomeAppsViewController: ResizablePopupViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updatePreferredContentSizeHeight(size: size)
-        view.addGestureRecognizer(resizeRecognizer)
-        resizeRecognizer.delegate = resizeGestureCoordinator
+        updatePreferredContentSizeHeight()
         
         pinnedAppModelController = PinnedHomeAppsModelController(collectionView: pinnedCollectionView)
         pinnedCollectionView.dataSource = pinnedAppModelController
@@ -69,6 +60,14 @@ final class HomeAppsViewController: ResizablePopupViewController {
             self?.noPinnedHintLabel.isHidden = !apps.isEmpty
         })
         
+        let pageInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        let spacing: CGFloat = {
+            let cellsWidth = candidateCollectionCellSize.width * CGFloat(cellCount.perRow)
+            let totalSpacing = AppDelegate.current.mainWindow.bounds.width - pageInset.horizontal - cellsWidth
+            return floor(totalSpacing / CGFloat(cellCount.perRow - 1))
+        }()
+        candidateCollectionLayout = HomeAppsFlowLayout(lineSpacing: 0, interitemSpacing: spacing, numberOfRows: cellCount.perColumn, numberOfColumns: cellCount.perRow, cellSize: candidateCollectionCellSize, pageInset: pageInset)
+        candidateCollectionView.collectionViewLayout = candidateCollectionLayout
         candidateAppModelController = CandidateHomeAppsModelController(collectionView: candidateCollectionView)
         candidateCollectionView.dataSource = candidateAppModelController
         candidateCollectionView.delegate = self
@@ -77,96 +76,45 @@ final class HomeAppsViewController: ResizablePopupViewController {
         candidateCollectionView.addInteraction(candidateAppModelController.dropInteraction)
         candidateAppModelController.reloadData(completion: { [weak self] (apps) in
             self?.setCandidateEmptyHintHidden(!apps.isEmpty)
+            self?.updateNumberOfPagesForPageControl(with: apps.count)
         })
-        
-        let window = AppDelegate.current.mainWindow
-        let maxHeight = window.bounds.height - window.safeAreaInsets.top
-        let candidateHeight = maxHeight - titleBarHeightConstraint.constant - pinnedWrapperHeightConstraint.constant
-        candidateCollectionViewHeightConstraint.constant = candidateHeight
+        candidateCollectionViewHeightConstraint.constant = candidateCollectionCellSize.height * CGFloat(cellCount.perColumn)
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateNoPinnedHint), name: AppGroupUserDefaults.User.homeAppIdsDidChangeNotification, object: nil)
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
-        let cellCount = CGFloat(cellCountPerRow)
-        let iconInset: CGFloat = 36
-        
-        let candidateInset = round((view.bounds.width - 2 * cellCount * iconInset - cellCount * pinnedCollectionLayout.itemSize.width) / (2 - 2 * cellCount))
-        candidateCollectionLayout.sectionInset.left = candidateInset
-        candidateCollectionLayout.sectionInset.right = candidateInset
-        let candidateCellsWidth = view.bounds.width
-            - candidateCollectionLayout.sectionInset.horizontal
-            - candidateCollectionLayout.minimumInteritemSpacing * CGFloat(cellCountPerRow - 1)
-        let candidateCellWidth = floor(candidateCellsWidth / CGFloat(cellCountPerRow))
-        candidateCollectionLayout.itemSize.width = candidateCellWidth
-        
-        let inset = candidateCollectionLayout.sectionInset.left
-            - pinnedCollectionViewLeadingConstraint.constant
-            + (candidateCollectionLayout.itemSize.width - pinnedCollectionLayout.itemSize.width) / 2
-        pinnedCollectionLayout.sectionInset.left = inset
-        pinnedCollectionLayout.sectionInset.right = inset
         let pinnedSpacing: CGFloat = {
-            let cellsWidth = pinnedCollectionLayout.itemSize.width * CGFloat(cellCountPerRow)
+            let cellsWidth = pinnedCollectionLayout.itemSize.width * CGFloat(cellCount.perRow)
             let totalSpacing = view.bounds.width
                 - pinnedCollectionViewLeadingConstraint.constant
                 - pinnedCollectionViewTrailingConstraint.constant
                 - pinnedCollectionLayout.sectionInset.horizontal
                 - cellsWidth
-            return floor(totalSpacing / CGFloat(cellCountPerRow))
+            return floor(totalSpacing / CGFloat(cellCount.perRow - 1))
         }()
         pinnedCollectionLayout.minimumInteritemSpacing = pinnedSpacing
     }
     
-    override func preferredContentHeight(forSize size: Size) -> CGFloat {
-        view.layoutIfNeeded()
-        return titleBarHeightConstraint.constant
-            + pinnedWrapperHeightConstraint.constant
-            + cellSize.height * CGFloat(cellCountPerPage.column)
-            + pageControlBottomConstraint.constant
-            + AppDelegate.current.mainWindow.safeAreaInsets.bottom
-            + 30
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        guard traitCollection.preferredContentSizeCategory != previousTraitCollection?.preferredContentSizeCategory else {
+            return
+        }
+        DispatchQueue.main.async {
+            self.updatePreferredContentSizeHeight()
+        }
     }
     
     @IBAction func pageControlValueChanged(_ sender: UIPageControl) {
-        
+        let x = CGFloat(pageControl.currentPage) * candidateCollectionView.frame.width
+        let offset = CGPoint(x: x, y: 0)
+        candidateCollectionView.setContentOffset(offset, animated: true)
     }
     
     @IBAction func dismissAction(_ sender: Any) {
         dismissAsChild(completion: nil)
-    }
-    
-    @objc func updateNoPinnedHint() {
-        noPinnedHintLabel.isHidden = !AppGroupUserDefaults.User.homeAppIds.isEmpty
-    }
-    
-    func setCandidateEmptyHintHidden(_ hidden: Bool) {
-        if hidden {
-            candidateEmptyHintLabelIfLoaded?.removeFromSuperview()
-        } else {
-            let x = candidateCollectionLayout.sectionInset.left
-            let y = candidateCollectionLayout.itemSize.height
-            let width = candidateCollectionView.bounds.width
-                - candidateCollectionLayout.sectionInset.horizontal
-            let height = preferredContentSize.height
-                - titleBarHeightConstraint.constant
-                - pinnedWrapperHeightConstraint.constant
-                - candidateCollectionLayout.itemSize.height
-            candidateEmptyHintLabel.frame = CGRect(x: x, y: y, width: width, height: round(height / 3 * 2))
-            if candidateEmptyHintLabel.superview == nil {
-                candidateCollectionView.addSubview(candidateEmptyHintLabel)
-            }
-        }
-    }
-
-    override func updatePreferredContentSizeHeight(size: ResizablePopupViewController.Size) {
-        guard !isBeingDismissed else {
-            return
-        }
-        let height = preferredContentHeight(forSize: size)
-        preferredContentSize.height = height
-        view.frame.origin.y = backgroundButton.bounds.height - height
     }
     
 }
@@ -192,15 +140,20 @@ extension HomeAppsViewController: UICollectionViewDelegate {
             present(vc, animated: true, completion: nil)
         }
     }
+    
+}
 
+extension HomeAppsViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(round(page))
+    }
+    
 }
 
 extension HomeAppsViewController {
-
-    @objc func backgroundTappingAction() {
-        dismissAsChild(completion: nil)
-    }
-
+    
     func dismissAsChild(completion: (() -> Void)?) {
         UIView.animate(withDuration: 0.3, animations: {
             self.view.frame.origin.y = self.backgroundButton.bounds.height
@@ -213,16 +166,16 @@ extension HomeAppsViewController {
             completion?()
         }
     }
-
+    
     func presentAsChild(of parent: UIViewController) {
         loadViewIfNeeded()
         backgroundButton.frame = parent.view.bounds
         backgroundButton.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
+        
         parent.addChild(self)
         parent.view.addSubview(backgroundButton)
         didMove(toParent: parent)
-
+        
         view.frame = CGRect(x: 0,
                             y: backgroundButton.bounds.height,
                             width: backgroundButton.bounds.width,
@@ -234,5 +187,58 @@ extension HomeAppsViewController {
             self.backgroundButton.backgroundColor = UIColor.black.withAlphaComponent(0.3)
         })
     }
-
+    
+    @objc private func backgroundTappingAction() {
+        dismissAsChild(completion: nil)
+    }
+    
+    @objc private func updateNoPinnedHint() {
+        noPinnedHintLabel.isHidden = !AppGroupUserDefaults.User.homeAppIds.isEmpty
+    }
+    
+    private func updateNumberOfPagesForPageControl(with dataCount: Int) {
+        let itemsPerPage = cellCount.perColumn * cellCount.perRow
+        let numberOfPages = ceil(Double(dataCount) / Double(itemsPerPage))
+        pageControl.numberOfPages = Int(numberOfPages)
+    }
+    
+    private func updatePreferredContentSizeHeight() {
+        guard !isBeingDismissed else {
+            return
+        }
+        let height = preferredContentHeight()
+        preferredContentSize.height = height
+        view.frame.origin.y = backgroundButton.bounds.height - height
+    }
+    
+    private func preferredContentHeight() -> CGFloat {
+        view.layoutIfNeeded()
+        return titleBarHeightConstraint.constant
+            + pinnedWrapperHeightConstraint.constant
+            + candidateCollectionViewHeightConstraint.constant
+            + pageControlTopConstraint.constant
+            + ceil(pageControl.bounds.height)
+            + AppDelegate.current.mainWindow.safeAreaInsets.bottom
+            + 22
+    }
+    
+    private func setCandidateEmptyHintHidden(_ hidden: Bool) {
+        if hidden {
+            candidateEmptyHintLabelIfLoaded?.removeFromSuperview()
+        } else {
+            let x = candidateCollectionLayout.sectionInset.left
+            let y = candidateCollectionLayout.itemSize.height
+            let width = candidateCollectionView.bounds.width
+                - candidateCollectionLayout.sectionInset.horizontal
+            let height = preferredContentSize.height
+                - titleBarHeightConstraint.constant
+                - pinnedWrapperHeightConstraint.constant
+                - candidateCollectionLayout.itemSize.height
+            candidateEmptyHintLabel.frame = CGRect(x: x, y: y, width: width, height: round(height / 3 * 2))
+            if candidateEmptyHintLabel.superview == nil {
+                candidateCollectionView.addSubview(candidateEmptyHintLabel)
+            }
+        }
+    }
+    
 }

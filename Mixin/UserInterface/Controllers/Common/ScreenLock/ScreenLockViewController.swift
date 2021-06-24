5b9e7ffe6cab2570143a2a2a9e14a79c22a0d69f
fanyu <fanyukobe@gmail.com>
2021-06-24 09:46:15 +0800

    Adapt blur effect with user interface's style automatically

--- Mixin/UserInterface/Controllers/Common/ScreenLock/ScreenLockViewController.swift
+++ Mixin/UserInterface/Controllers/Common/ScreenLock/ScreenLockViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ScreenLockViewController: UIViewController {
-
+    
     @IBOutlet weak var unlockTipLabel: UILabel!
     @IBOutlet weak var unlockButton: UIButton!
     @IBOutlet weak var contentView: UIView!
@@ -11,25 +11,14 @@ class ScreenLockViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateBackgroundViewEffect()
+        backgroundView.effect = .regularBlur
         contentView.isHidden = true
         unlockTipLabel.text = R.string.localizable.screen_lock_unlock_tip(biometryType.localizedName)
         unlockButton.setTitle(R.string.localizable.screen_lock_unlock_button_title(biometryType.localizedName), for: .normal)
     }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if previousTraitCollection?.userInterfaceStyle != traitCollection.userInterfaceStyle {
-            updateBackgroundViewEffect()
-        }
-    }
     
     func showUnlockOption(_ show: Bool) {
         contentView.isHidden = !show
-    }
-    
-    private func updateBackgroundViewEffect() {
-        backgroundView.effect = UserInterfaceStyle.current == .light ? .lightBlur : .darkBlur
     }
     
     @IBAction func tapUnlockButtonAction(_ sender: Any) {

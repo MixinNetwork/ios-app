960e0a3669ed47b39b7b5c4a480f4aa38210662e
wuyueyang <wuyueyang@mixin.one>
2021-07-13 21:07:46 +0800

    Update string keys

--- Mixin/UserInterface/Controllers/Chat/ConversationViewController.swift
+++ Mixin/UserInterface/Controllers/Chat/ConversationViewController.swift
@@ -2658,7 +2658,7 @@ extension ConversationViewController {
                     if viewModel.message.appCard?.isShareable ?? true {
                         return .available
                     } else {
-                        let reason = R.string.localizable.chat_transcript_forward_invalid_app_card_not_shareable()
+                        let reason = R.string.localizable.chat_forward_invalid_app_card_not_shareable()
                         return .visibleButUnavailable(reason: reason)
                     }
                 } else {

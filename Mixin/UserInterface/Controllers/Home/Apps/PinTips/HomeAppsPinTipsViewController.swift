989c00a6b19b9a93318f201028bb3433abef3e6f
fanyu <fanyukobe@gmail.com>
2021-07-14 19:31:55 +0800

    Update pin tips dot line layout

--- Mixin/UserInterface/Controllers/Home/Apps/PinTips/HomeAppsPinTipsViewController.swift
+++ Mixin/UserInterface/Controllers/Home/Apps/PinTips/HomeAppsPinTipsViewController.swift
@@ -8,6 +8,7 @@ class HomeAppsPinTipsViewController: UIViewController {
     @IBOutlet weak var pinnedAppViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var candidateAppLeadingConstraint: NSLayoutConstraint!
     @IBOutlet var pinnedAppSpaceConstraints: [NSLayoutConstraint]!
+    @IBOutlet weak var lineViewTrailingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +16,7 @@ class HomeAppsPinTipsViewController: UIViewController {
         candidateAppLeadingConstraint.constant = mode.sectionInset.left + mode.itemSize.width + mode.minimumInteritemSpacing + (mode.itemSize.width - HomeAppsMode.imageContainerSize.width)/2
         pinnedAppSpaceConstraints.forEach({ $0.constant = HomeAppsMode.pinned.minimumInteritemSpacing })
         pinnedAppViewTopConstraint.constant = topSpace
+        lineViewTrailingConstraint.constant = 3 - HomeAppsMode.pinned.itemSize.width/2
     }
     
     @IBAction func dismiss(_ sender: Any) {

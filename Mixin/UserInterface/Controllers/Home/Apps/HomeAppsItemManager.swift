0cf1cb812ff6d11507a09ed0d98f527ccdd8cb8d
fanyu <fanyukobe@gmail.com>
2021-07-22 11:31:02 +0800

    Optimized data

--- Mixin/UserInterface/Controllers/Home/Apps/HomeAppsItemManager.swift
+++ Mixin/UserInterface/Controllers/Home/Apps/HomeAppsItemManager.swift
@@ -71,9 +71,9 @@ extension HomeAppsItemManager {
             if let parsedDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? JSONDictionary,
                let pinnedJSONArray = parsedDict["pinned"] as? JSONArray,
                let pagesJSONArray = parsedDict["pages"] as? [JSONArray] {
-                pinnedItems = pinnedAppItems(from: pinnedJSONArray, allAppIds: allAppIds)
+                pinnedItems = appItems(from: pinnedJSONArray, allAppIds: allAppIds, embeddedApps: embeddedApps, appUsers: appUsers)
                 candidateItems = pagesJSONArray.compactMap({ jsonArray -> [AppItem]? in
-                    let page = candidateAppItems(from: jsonArray, allAppIds: allAppIds, embeddedApps: embeddedApps, appUsers: appUsers)
+                    let page = appItems(from: jsonArray, allAppIds: allAppIds, embeddedApps: embeddedApps, appUsers: appUsers)
                     return page.count > 0 ? page : nil
                 })
                 // apped newly added apps
@@ -93,33 +93,8 @@ extension HomeAppsItemManager {
             initItems()
         }
     }
-    
-    private func pinnedAppItems(from jsonArray: JSONArray, allAppIds: [String]) -> [AppItem] {
-        return jsonArray.compactMap { item -> AppItem? in
-            guard let typeID = item["type"] as? Int, let type = HomeAppItemType(rawValue: typeID), type == .app else {
-                return nil
-            }
-            guard let id = item["id"] as? String, allAppIds.contains(id), !existsItemIds.contains(id) else {
-                return nil
-            }
-            existsItemIds.insert(id)
-            return AppModel(id: id, app: HomeApp(id: id)!)
-        }
-    }
-    
-    private func candidateAppItems(from jsonArray: JSONArray, allAppIds: [String], embeddedApps: [EmbeddedApp], appUsers: [User]) -> [AppItem] {
-        let pinnedIds = pinnedItems.compactMap({ ($0 as? AppModel)?.id })
-        var candidateEmbeddedApps = embeddedApps
-        candidateEmbeddedApps.removeAll(where: {
-            pinnedIds.contains($0.id)
-        })
-        let candidateAppUsers = appUsers.filter { (user) -> Bool in
-            if let id = user.appId {
-                return !pinnedIds.contains(id)
-            } else {
-                return false
-            }
-        }
+
+    private func appItems(from jsonArray: JSONArray, allAppIds: [String], embeddedApps: [EmbeddedApp], appUsers: [User]) -> [AppItem] {
         return jsonArray.compactMap { item -> AppItem? in
             guard let typeID = item["type"] as? Int, let type = HomeAppItemType(rawValue: typeID) else {
                 return nil
@@ -130,9 +105,9 @@ extension HomeAppsItemManager {
                     return nil
                 }
                 existsItemIds.insert(id)
-                if let embeddedApp = candidateEmbeddedApps.first(where: { $0.id == id }) {
+                if let embeddedApp = embeddedApps.first(where: { $0.id == id }) {
                     return AppModel(id: id, app: .embedded(embeddedApp))
-                } else if let appUser = candidateAppUsers.first(where: { $0.appId == id }) {
+                } else if let appUser = appUsers.first(where: { $0.appId == id }) {
                     return AppModel(id: id, app: .external(appUser))
                 } else {
                     return nil
@@ -147,9 +122,9 @@ extension HomeAppsItemManager {
                             return nil
                         }
                         existsItemIds.insert(id)
-                        if let embeddedApp = candidateEmbeddedApps.first(where: { $0.id == id }) {
+                        if let embeddedApp = embeddedApps.first(where: { $0.id == id }) {
                             return AppModel(id: id, app: .embedded(embeddedApp))
-                        } else if let appUser = candidateAppUsers.first(where: { $0.appId == id }) {
+                        } else if let appUser = appUsers.first(where: { $0.appId == id }) {
                             return AppModel(id: id, app: .external(appUser))
                         } else {
                             return nil

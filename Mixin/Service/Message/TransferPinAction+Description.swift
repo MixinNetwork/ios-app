fedaa4e62d2962a5698d5f41a03df68a7f811241
fanyu <fanyukobe@gmail.com>
2021-08-27 18:27:06 +0800

    Remove pin preview

--- Mixin/Service/Message/TransferPinAction+Description.swift
+++ Mixin/Service/Message/TransferPinAction+Description.swift
@@ -40,34 +40,5 @@ extension TransferPinAction {
         }
         return getPinMessage(userId: userId, userFullName: userFullName, category: localContent.category, content: localContent.content)
     }
-    
-    static func getPinPreview(userId: String, userFullName: String, category: String, content: String?) -> String {
-        let uFullName = userId == myUserId ? R.string.localizable.chat_message_you() : userFullName
-        if category.hasSuffix("_TEXT"), let content = content {
-            return R.string.localizable.chat_pinned_preview_text_message(uFullName, content)
-        } else if category.hasSuffix("_AUDIO") {
-            return R.string.localizable.chat_pinned_preview_audio_message(uFullName)
-        } else if category.hasSuffix("_IMAGE") {
-            return R.string.localizable.chat_pinned_preview_image_message(uFullName)
-        } else if category.hasSuffix("_VIDEO") {
-            return R.string.localizable.chat_pinned_preview_video_message(uFullName)
-        } else if category.hasSuffix("_LIVE") {
-            return R.string.localizable.chat_pinned_preview_live_message(uFullName)
-        } else if category.hasSuffix("_STICKER") {
-            return R.string.localizable.chat_pinned_preview_sticker_message(uFullName)
-        } else if category.hasSuffix("_DATA") {
-            return R.string.localizable.chat_pinned_preview_data_message(uFullName)
-        } else if category.hasSuffix("_CONTACT") {
-            return R.string.localizable.chat_pinned_preview_contact_message(uFullName)
-        } else if category.hasSuffix("_POST") {
-            return R.string.localizable.chat_pinned_preview_post_message(uFullName)
-        } else if category.hasSuffix("_LOCATION") {
-            return R.string.localizable.chat_pinned_preview_location_message(uFullName)
-        } else if category.hasSuffix("_TRANSCRIPT") {
-            return R.string.localizable.chat_pinned_preview_transcript_message(uFullName)
-        } else {
-            return R.string.localizable.chat_pinned_preview_general_message(uFullName)
-        }
-    }
-    
+        
 }

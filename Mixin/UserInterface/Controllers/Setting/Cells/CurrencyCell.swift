8746824436c951fba158c6b4ff508d4e963c8018
wuyueyang <wuyueyang@mixin.one>
2021-09-14 23:50:05 +0800

    Add shadow behind currency icon

--- Mixin/UserInterface/Controllers/Setting/Cells/CurrencyCell.swift
+++ Mixin/UserInterface/Controllers/Setting/Cells/CurrencyCell.swift
@@ -7,9 +7,55 @@ class CurrencyCell: UITableViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var codeLabel: UILabel!
     
+    private var iconLayer: CALayer {
+        iconImageView.layer
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        iconLayer.shadowOpacity = 1
+        iconLayer.shadowRadius = 8
+        iconLayer.shadowOffset = CGSize(width: 0, height: 2)
+        updateShadowColor()
+    }
+    
+    override func layoutSubviews() {
+        let iconFrameBefore = iconImageView.frame
+        super.layoutSubviews()
+        if iconImageView.frame != iconFrameBefore, let icon = iconImageView.image {
+            updateShadowPath(iconSize: icon.size)
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateShadowColor()
+    }
+    
     func render(currency: Currency) {
-        iconImageView.image = currency.icon
+        let icon = currency.icon
+        iconImageView.image = icon
+        updateShadowPath(iconSize: icon.size)
         codeLabel.text = currency.code + " (" + currency.symbol + ")"
+    }
+    
+    private func updateShadowColor() {
+        switch UserInterfaceStyle.current {
+        case .light:
+            iconLayer.shadowColor = UIColor.black.withAlphaComponent(0.12).cgColor
+        case .dark:
+            iconLayer.shadowColor = UIColor.black.withAlphaComponent(0.20).cgColor
+        }
+    }
+    
+    private func updateShadowPath(iconSize: CGSize) {
+        let shadowOrigin = CGPoint(x: (iconImageView.bounds.width - iconSize.width) / 2,
+                                   y: (iconImageView.bounds.height - iconSize.height) / 2)
+        iconLayer.shadowPath = CGPath(roundedRect: CGRect(origin: shadowOrigin, size: iconSize),
+                                      cornerWidth: iconSize.width / 2,
+                                      cornerHeight: iconSize.height / 2,
+                                      transform: nil)
+        
     }
     
 }

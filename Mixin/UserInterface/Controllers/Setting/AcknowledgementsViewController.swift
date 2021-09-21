d9fc42edf277f3448f0d355f9d5c510e6ed7e63a
wuyueyang <wuyueyang@mixin.one>
2021-09-21 17:14:48 +0800

    Fix app crash

--- Mixin/UserInterface/Controllers/Setting/AcknowledgementsViewController.swift
+++ Mixin/UserInterface/Controllers/Setting/AcknowledgementsViewController.swift
@@ -4,15 +4,15 @@ import AcknowList
 class AcknowledgementsViewController: AcknowListViewController {
     
     class func instance() -> UIViewController {
-        let customAcknows: [Acknow] = {
-            guard let url = Bundle.main.url(forResource: "Custom-acknowledgements", withExtension: "plist") else {
+        let filenames = ["Pods-Mixin-acknowledgements", "Custom-acknowledgements"]
+        let acknows: [Acknow] = filenames.flatMap { filename -> [Acknow] in
+            guard let url = Bundle.main.url(forResource: filename, withExtension: "plist") else {
                 return []
             }
             let parser = AcknowParser(plistPath: url.path)
             return parser.parseAcknowledgements()
-        }()
-        let controller = AcknowledgementsViewController()
-        controller.acknowledgements.append(contentsOf: customAcknows)
+        }
+        let controller = AcknowledgementsViewController(acknowledgements: acknows)
         let title = R.string.localizable.about_acknowledgements()
         return ContainerViewController.instance(viewController: controller, title: title)
     }

1b40f2a036e109c4dd41e8e01fd3c0bae50af44c
wuyueyang <wuyueyang@mixin.one>
2021-10-20 18:26:56 +0800

    Test photo sizes

--- Mixin/UserInterface/Controllers/Chat/Model/PhotoRepresentableMessageViewModel.swift
+++ Mixin/UserInterface/Controllers/Chat/Model/PhotoRepresentableMessageViewModel.swift
@@ -9,14 +9,6 @@ class PhotoRepresentableMessageViewModel: ImageMessageViewModel {
     var layoutPosition = VerticalPositioningImageView.Position.center
     var expandIconOrigin: CGPoint?
     
-    private struct PresentationSize {
-        static let maxHeight: CGFloat = 280
-        static let maxWidth: CGFloat = 210
-        static let minHeight: CGFloat = 120
-        static let minWidth: CGFloat = 120
-        static let maxRatio: CGFloat = maxHeight / maxWidth
-    }
-    
     override init(message: MessageItem) {
         let mediaWidth = abs(CGFloat(message.mediaWidth ?? 0))
         let mediaHeight = abs(CGFloat(message.mediaHeight ?? 0))
@@ -29,47 +21,7 @@ class PhotoRepresentableMessageViewModel: ImageMessageViewModel {
     }
     
     override func layout(width: CGFloat, style: MessageViewModel.Style) {
-        let ratio = contentSize.height / contentSize.width
-        let scaledHeight: CGFloat
-        let scaledWidth: CGFloat
-        if contentSize.width > PresentationSize.maxWidth && contentSize.height > PresentationSize.maxHeight {
-            if ratio > 1 {
-                if ratio > PresentationSize.maxRatio {
-                    scaledHeight = PresentationSize.maxHeight
-                    scaledWidth = round(scaledHeight / ratio)
-                } else {
-                    scaledWidth = PresentationSize.maxWidth
-                    scaledHeight = round(scaledWidth * ratio)
-                }
-            } else {
-                scaledWidth = PresentationSize.maxWidth
-                scaledHeight = max(PresentationSize.minHeight, round(scaledWidth * ratio))
-            }
-        } else if contentSize.height > PresentationSize.maxHeight && contentSize.width < PresentationSize.maxWidth {
-            scaledHeight = PresentationSize.maxHeight
-            scaledWidth = max(PresentationSize.minWidth, round(scaledHeight / ratio))
-        } else if contentSize.width > PresentationSize.maxWidth && contentSize.height < PresentationSize.maxHeight {
-            scaledWidth = PresentationSize.maxWidth
-            scaledHeight = max(PresentationSize.minHeight, round(scaledWidth * ratio))
-        } else if contentSize.width > PresentationSize.minWidth && contentSize.height < PresentationSize.minHeight {
-            scaledHeight = PresentationSize.minHeight
-            scaledWidth = min(PresentationSize.maxWidth, round(scaledHeight / ratio))
-        } else if contentSize.height > PresentationSize.minHeight && contentSize.width < PresentationSize.minWidth {
-            scaledWidth = PresentationSize.minWidth
-            scaledHeight = min(PresentationSize.maxHeight, round(scaledWidth * ratio))
-        } else if contentSize.height < PresentationSize.minHeight && contentSize.width < PresentationSize.minWidth {
-            if ratio > 1 {
-                scaledWidth = PresentationSize.minWidth
-                scaledHeight = min(PresentationSize.maxHeight, round(scaledWidth * ratio))
-            } else {
-                scaledHeight = PresentationSize.minHeight
-                scaledWidth = min(PresentationSize.maxWidth, round(scaledHeight / ratio))
-            }
-        } else {
-            scaledWidth = contentSize.width
-            scaledHeight = contentSize.height
-        }
-        photoFrame.size = CGSize(width: scaledWidth, height: scaledHeight)
+        photoFrame.size = PhotoSizeCalculator.displaySize(for: contentSize)
         super.layout(width: width, style: style)
         layoutTrailingInfoBackgroundFrame()
         if imageWithRatioMaybeAnArticle(contentSize) {

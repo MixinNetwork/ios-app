550d981ce4aa593af0e2008ef9255c78d4388a58
wuyueyang <wuyueyang@mixin.one>
2022-01-13 20:47:52 +0800

    Apply code style

--- Mixin/UserInterface/Controllers/Setting/DeleteAccountVerifyPINViewController.swift
+++ Mixin/UserInterface/Controllers/Setting/DeleteAccountVerifyPINViewController.swift
@@ -4,10 +4,6 @@ class DeleteAccountVerifyPINViewController: VerifyPinViewController {
     
     var onSuccess: (() -> Void)?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     override func pinIsVerified(pin: String) {
         pinField.resignFirstResponder()
         dismiss(animated: true, completion: onSuccess)

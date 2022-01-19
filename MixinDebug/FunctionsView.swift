9111a6af22cd4f031219013f19b99f8ccec683b6
wuyueyang <wuyueyang@mixin.one>
2022-01-20 00:18:38 +0800

    Add PINIteratorTest

--- MixinDebug/FunctionsView.swift
+++ MixinDebug/FunctionsView.swift
@@ -13,32 +13,32 @@ struct FunctionsView: View {
     @ObservedObject private var server = AppGroupServer()
     
     var body: some View {
-        Form {
-            Section {
-                Toggle("AppGroup Server", isOn: $server.isOn)
-                    .disabled(server.isBusy)
-                if let address = server.address {
-                    Button {
-                        UIPasteboard.general.string = address
-                    } label: {
-                        Text(address)
-                            .foregroundColor(Color(UIColor.label))
+        NavigationView {
+            List {
+                Section {
+                    Toggle("AppGroup Server", isOn: $server.isOn)
+                        .disabled(server.isBusy)
+                    if let address = server.address {
+                        Button {
+                            UIPasteboard.general.string = address
+                        } label: {
+                            Text(address)
+                                .foregroundColor(Color(UIColor.label))
+                        }
+                    }
+                } footer: {
+                    if server.address != nil {
+                        Text("Tap to copy address")
                     }
                 }
-            } footer: {
-                if server.address != nil {
-                    Text("Tap to copy address")
+                
+                Section {
+                    NavigationLink("PIN Iterator Test", destination: PINIteratorView())
                 }
             }
+            .navigationTitle("Mixin DebugKit")
+            .listStyle(GroupedListStyle())
         }
-    }
-    
-}
-
-struct ContentView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        FunctionsView()
     }
     
 }

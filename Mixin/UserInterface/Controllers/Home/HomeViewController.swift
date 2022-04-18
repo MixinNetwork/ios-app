a4b319e2aca684749e0a771830695a16e1a431d9
wuyueyang <wuyueyang@mixin.one>
2022-04-18 15:35:54 +0800

    Remove CleanUpUnusedAttachmentJob

--- Mixin/UserInterface/Controllers/Home/HomeViewController.swift
+++ Mixin/UserInterface/Controllers/Home/HomeViewController.swift
@@ -142,7 +142,6 @@ class HomeViewController: UIViewController {
         Logger.general.info(category: "HomeViewController", message: "View did load with app state: \(UIApplication.shared.applicationStateString)")
         if UIApplication.shared.applicationState != .background {
             ConcurrentJobQueue.shared.addJob(job: RefreshAccountJob())
-            ConcurrentJobQueue.shared.addJob(job: CleanUpUnusedAttachmentJob())
             if AppGroupUserDefaults.User.hasRecoverMedia {
                 ConcurrentJobQueue.shared.addJob(job: RecoverMediaJob())
             }

991c4c51441a3feee7a2d5881c3f46ff7eff250c
wuyueyang <wuyueyang@mixin.one>
2022-05-04 17:01:12 +0800

    Fix message preview in home shows nothing for posts with newlines on start

--- MixinServices/MixinServices/Foundation/MarkdownControlCodeRemovable.swift
+++ MixinServices/MixinServices/Foundation/MarkdownControlCodeRemovable.swift
@@ -13,20 +13,21 @@ extension MarkdownControlCodeRemovable {
         guard isPostContent, let content = contentBeforeRemovingMarkdownControlCode else {
             return contentBeforeRemovingMarkdownControlCode ?? ""
         }
-        var endIndex = content.endIndex
+        let trimmedContent = content.trimmingCharacters(in: .whitespacesAndNewlines)
+        var endIndex = trimmedContent.endIndex
         var suffix = ""
-        for (index, char) in content.enumerated().lazy {
+        for (index, char) in trimmedContent.enumerated().lazy {
             let didMeetLineBreak = char == "\n"
             let didReachMaxLength = index == 60
             if didMeetLineBreak || didReachMaxLength {
-                endIndex = content.index(content.startIndex, offsetBy: index)
+                endIndex = trimmedContent.index(trimmedContent.startIndex, offsetBy: index)
                 if didReachMaxLength {
                     suffix = "..."
                 }
                 break
             }
         }
-        let fragment = String(content[content.startIndex..<endIndex])
+        let fragment = String(trimmedContent[content.startIndex..<endIndex])
         let text = MarkdownConverter.plainText(from: fragment)
         return text + suffix
     }

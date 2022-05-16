883e992358af3ce0fa5e39fb86ee5e6ddbe89e04
wuyueyang <wuyueyang@mixin.one>
2022-05-16 17:39:44 +0800

    Open link in search result

---
+++ Mixin/UserInterface/Controllers/Home/Cells/TopResultCell.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class TopResultCell: UITableViewCell {
+    
+    @IBOutlet weak var labelBackgroundView: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: ActivityIndicatorView!
+    
+    private var prefixAttributes: [NSAttributedString.Key: Any] {
+        [.font: UIFont.preferredFont(forTextStyle: .subheadline), .foregroundColor: UIColor.text]
+    }
+    
+    private var contentAttributes: [NSAttributedString.Key: Any] {
+        [.font: UIFont.preferredFont(forTextStyle: .subheadline), .foregroundColor: UIColor.theme]
+    }
+    
+    var isBusy = false {
+        didSet {
+            activityIndicator.isAnimating = isBusy
+        }
+    }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        let color: UIColor = highlighted ? .selectionBackground : .background
+        let work = {
+            self.labelBackgroundView.backgroundColor = color
+        }
+        if animated {
+            UIView.animate(withDuration: 0.3, animations: work)
+        } else {
+            work()
+        }
+    }
+    
+    func setText(number: String) {
+        setText(prefix: R.string.localizable.search_placeholder_number(), content: number)
+    }
+    
+    func setText(url: URL) {
+        setText(prefix: R.string.localizable.search_result_prefix_link(), content: url.absoluteString)
+    }
+    
+    private func setText(prefix: String, content: String) {
+        let attributedText = NSMutableAttributedString(string: prefix, attributes: prefixAttributes)
+        attributedText.append(NSAttributedString(string: content, attributes: contentAttributes))
+        label.attributedText = attributedText
+    }
+    
+}

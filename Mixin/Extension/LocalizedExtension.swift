947e90364d281cf8b44ba0a80c093aad990a3ee1
fanyu <fanyukobe@gmail.com>
2022-05-17 15:09:44 +0800

    Apply code style

--- Mixin/Extension/LocalizedExtension.swift
+++ Mixin/Extension/LocalizedExtension.swift
@@ -2,7 +2,7 @@ import UIKit
 
 public func LocalizedString(_ key: String, comment: String) -> String {
     let localText = NSLocalizedString(key, comment: "")
-    return localText == key ? comment :  localText
+    return localText == key ? comment : localText
 }
 
 extension UIButton {

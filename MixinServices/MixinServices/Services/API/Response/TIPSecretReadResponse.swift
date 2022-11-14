3bbda58a815d063ca20e073f9e8c5468df0a05e4
wuyueyang <wuyueyang@mixin.one>
2022-11-14 16:00:36 +0800

    Remove local TIP Priv if reads an invalid secret

--- MixinServices/MixinServices/Services/API/Response/TIPSecretReadResponse.swift
+++ MixinServices/MixinServices/Services/API/Response/TIPSecretReadResponse.swift
@@ -15,6 +15,10 @@ extension TIPSecretReadResponse: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let encoded = try container.decode(String.self, forKey: .seed)
+        guard !encoded.isEmpty else {
+            let context = DecodingError.Context(codingPath: [CodingKeys.seed], debugDescription: "Empty seed")
+            throw DecodingError.dataCorrupted(context)
+        }
         if let decoded = Data(base64URLEncoded: encoded) {
             self.seed = decoded
         } else {

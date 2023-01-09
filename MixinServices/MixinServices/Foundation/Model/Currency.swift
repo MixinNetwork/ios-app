c307c892ef7cc383435065f77b5db11f2fd86ee5
wuyueyang <wuyueyang@mixin.one>
2023-01-09 16:51:56 +0800

    Update rates

--- MixinServices/MixinServices/Foundation/Model/Currency.swift
+++ MixinServices/MixinServices/Foundation/Model/Currency.swift
@@ -35,19 +35,19 @@ public extension Currency {
     private(set) static var all: [Currency] = {
         let currencies = [
             Currency(code: "USD", symbol: "$", rate: 1),
-            Currency(code: "CNY", symbol: "¥", rate: 6.98),
-            Currency(code: "JPY", symbol: "¥", rate: 137.1),
-            Currency(code: "EUR", symbol: "€", rate: 0.953886),
-            Currency(code: "KRW", symbol: "₩", rate: 1316.49),
-            Currency(code: "HKD", symbol: "HK$", rate: 7.78),
-            Currency(code: "GBP", symbol: "£", rate: 0.820904),
-            Currency(code: "AUD", symbol: "A$", rate: 1.49),
-            Currency(code: "SGD", symbol: "S$", rate: 1.36),
-            Currency(code: "MYR", symbol: "RM", rate: 4.38),
-            Currency(code: "PHP", symbol: "₱", rate: 56.0),
-            Currency(code: "AED", symbol: "AED ", rate: 3.67),
-            Currency(code: "TWD", symbol: "NT$", rate: 30.63),
-            Currency(code: "CAD", symbol: "C$", rate: 1.37),
+            Currency(code: "CNY", symbol: "¥", rate: 6.7708938),
+            Currency(code: "JPY", symbol: "¥", rate: 132.1074382),
+            Currency(code: "EUR", symbol: "€", rate: 0.9351964),
+            Currency(code: "KRW", symbol: "₩", rate: 1244.0342125),
+            Currency(code: "HKD", symbol: "HK$", rate: 7.8110311),
+            Currency(code: "GBP", symbol: "£", rate: 0.8230246),
+            Currency(code: "AUD", symbol: "A$", rate: 1.4401901),
+            Currency(code: "SGD", symbol: "S$", rate: 1.3301756),
+            Currency(code: "MYR", symbol: "RM", rate: 4.3705769),
+            Currency(code: "PHP", symbol: "₱", rate: 54.9272504),
+            Currency(code: "AED", symbol: "AED ", rate: 3.6704845),
+            Currency(code: "TWD", symbol: "NT$", rate: 30.5140279),
+            Currency(code: "CAD", symbol: "C$", rate: 1.3401769),
         ]
         let rates = AppGroupUserDefaults.currencyRates
         for currency in currencies {

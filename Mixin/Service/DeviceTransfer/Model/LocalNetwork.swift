0dc4507d4c0588e4bfcb1338525d919ced67fd0b
fanyu <fanyukobe@gmail.com>
2023-04-17 20:20:30 +0800

    Optimize the local authorization process

--- Mixin/Service/DeviceTransfer/Model/LocalNetwork.swift
+++ Mixin/Service/DeviceTransfer/Model/LocalNetwork.swift
@@ -1,36 +1,45 @@
 import Foundation
 import Network
+import MixinServices
 
 enum LocalNetwork {
     
     static func requestAuthorization(completion: @escaping (Bool) -> Void) {
-        guard let ip = IPAddress.currentWiFiIPAddress() else {
-            completion(false)
-            return
-        }
-        let host = NWEndpoint.Host(ip)
-        let port = NWEndpoint.Port(integerLiteral: NetworkPort.randomAvailablePort())
-        let endPoint = NWEndpoint.hostPort(host: host, port: port)
-        let connection = NWConnection(to: endPoint, using: .tcp)
-        connection.stateUpdateHandler = { newState in
-            switch newState {
-            case .waiting(let error):
-                if error == .posix(.ENETDOWN) {
-                    completion(false)
-                } else {
-                    completion(true)
-                }
-                connection.stateUpdateHandler = nil
-                connection.forceCancel()
-            case .ready, .failed:
-                connection.stateUpdateHandler = nil
-                connection.forceCancel()
-                completion(true)
-            default:
-                break
+        if #available(iOS 14.0, *) {
+            guard let ip = IPAddress.currentWiFiIPAddress() else {
+                completion(false)
+                return
             }
+            let host = NWEndpoint.Host(ip)
+            let port = NWEndpoint.Port(integerLiteral: NetworkPort.randomAvailablePort())
+            let endPoint = NWEndpoint.hostPort(host: host, port: port)
+            let connection = NWConnection(to: endPoint, using: .tcp)
+            if AppGroupUserDefaults.isLocalNetworkTriggered {
+                connection.stateUpdateHandler = { newState in
+                    switch newState {
+                    case .waiting(let error):
+                        if error == .posix(.ENETDOWN) {
+                            completion(false)
+                        } else {
+                            completion(true)
+                        }
+                        connection.stateUpdateHandler = nil
+                        connection.forceCancel()
+                    case .ready, .failed:
+                        connection.stateUpdateHandler = nil
+                        connection.forceCancel()
+                        completion(true)
+                    default:
+                        break
+                    }
+                }
+            } else {
+                AppGroupUserDefaults.isLocalNetworkTriggered = true
+            }
+            connection.start(queue: .main)
+        } else {
+            completion(true)
         }
-        connection.start(queue: .main)
     }
     
 }

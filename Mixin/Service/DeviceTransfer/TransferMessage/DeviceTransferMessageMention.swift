7c5f9c1cf53fe7930eba014f0a4eed772b64d5f7
fanyu <fanyukobe@gmail.com>
2023-04-21 19:16:38 +0800

    Send mentions as json string

--- Mixin/Service/DeviceTransfer/TransferMessage/DeviceTransferMessageMention.swift
+++ Mixin/Service/DeviceTransfer/TransferMessage/DeviceTransferMessageMention.swift
@@ -5,18 +5,18 @@ struct DeviceTransferMessageMention {
     
     let conversationId: String
     let messageId: String
-    let mentionsJson: Data?
+    let mentions: String?
     let hasRead: Bool
     
     init(messageMention: MessageMention) {
         conversationId = messageMention.conversationId
         messageId = messageMention.messageId
-        mentionsJson = messageMention.mentionsJson
+        mentions = String(data: messageMention.mentionsJson, encoding: .utf8)
         hasRead = messageMention.hasRead
     }
     
     func toMessageMention() -> MessageMention? {
-        if let mentionsJson {
+        if let mentions, let mentionsJson = mentions.data(using: .utf8) {
             return MessageMention(conversationId: conversationId,
                                   messageId: messageId,
                                   mentionsJson: mentionsJson,
@@ -39,7 +39,7 @@ extension DeviceTransferMessageMention: Codable {
     enum CodingKeys: String, CodingKey {
         case messageId = "message_id"
         case conversationId = "conversation_id"
-        case mentionsJson = "mentions"
+        case mentions
         case hasRead = "has_read"
     }
     

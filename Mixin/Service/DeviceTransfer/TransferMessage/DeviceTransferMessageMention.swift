43c37e2351f21aa8f3a4adf5d6837db9833b55c4
fanyu <fanyukobe@gmail.com>
2023-04-24 18:52:25 +0800

    Remove mentions

--- Mixin/Service/DeviceTransfer/TransferMessage/DeviceTransferMessageMention.swift
+++ Mixin/Service/DeviceTransfer/TransferMessage/DeviceTransferMessageMention.swift
@@ -5,23 +5,16 @@ struct DeviceTransferMessageMention {
     
     let conversationId: String
     let messageId: String
-    let mentions: String?
     let hasRead: Bool
     
     init(messageMention: MessageMention) {
         conversationId = messageMention.conversationId
         messageId = messageMention.messageId
-        mentions = String(data: messageMention.mentionsJson, encoding: .utf8)
         hasRead = messageMention.hasRead
     }
     
     func toMessageMention() -> MessageMention? {
-        if let mentions, let mentionsJson = mentions.data(using: .utf8) {
-            return MessageMention(conversationId: conversationId,
-                                  messageId: messageId,
-                                  mentionsJson: mentionsJson,
-                                  hasRead: hasRead)
-        } else if let content = MessageDAO.shared.messageContent(conversationId: conversationId, messageId: messageId) {
+        if let content = MessageDAO.shared.messageContent(conversationId: conversationId, messageId: messageId) {
             return MessageMention(conversationId: conversationId,
                                   messageId: messageId,
                                   content: content,
@@ -39,7 +32,6 @@ extension DeviceTransferMessageMention: Codable {
     enum CodingKeys: String, CodingKey {
         case messageId = "message_id"
         case conversationId = "conversation_id"
-        case mentions
         case hasRead = "has_read"
     }
     

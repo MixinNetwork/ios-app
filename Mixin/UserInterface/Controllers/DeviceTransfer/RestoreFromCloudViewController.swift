4e9bb18c523f5dfb96a7c901798ff89299ff3300
fanyu <fanyukobe@gmail.com>
2023-04-25 15:48:37 +0800

    Asynchronous database querying

--- Mixin/UserInterface/Controllers/DeviceTransfer/RestoreFromCloudViewController.swift
+++ Mixin/UserInterface/Controllers/DeviceTransfer/RestoreFromCloudViewController.swift
@@ -3,9 +3,11 @@ import MixinServices
 
 class RestoreFromCloudViewController: DeviceTransferSettingViewController {
     
-    private lazy var dataSource = SettingsDataSource(sections: [
-        SettingsSection(rows: [SettingsRow(title: R.string.localizable.restore_chat_history(), titleStyle: .highlighted)])
-    ])
+    private let section = SettingsRadioSection(rows: [SettingsRow(title: R.string.localizable.restore_chat_history(), titleStyle: .highlighted)])
+    
+    private lazy var dataSource = SettingsDataSource(sections: [section])
+    
+    private var isQuerying = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,18 +28,45 @@ extension RestoreFromCloudViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        guard !isQuerying else {
+            return
+        }
         let databasePath = AppGroupContainer.userDatabaseUrl.path
         let databaseExists = FileManager.default.fileExists(atPath: databasePath)
-        if databaseExists, let lastMessageCreatedAt = MessageDAO.shared.lastMessageCreatedAt() {
-            let messageCount = MessageDAO.shared.messagesCount()
-            let createdAt = DateFormatter.dateFull.string(from: lastMessageCreatedAt.toUTCDate())
-            alert(R.string.localizable.restore_from_icloud_confirmation(messageCount, createdAt), actionTitle: R.string.localizable.overwrite()) { _ in
-                self.restoreFromCloud()
+        if databaseExists {
+            isQuerying = true
+            section.setAccessory(.busy, forRowAt: indexPath.row)
+            DispatchQueue.global().async {
+                if let lastMessageCreatedAt = MessageDAO.shared.lastMessageCreatedAt() {
+                    let messageCount = MessageDAO.shared.messagesCount()
+                    let createdAt = DateFormatter.dateFull.string(from: lastMessageCreatedAt.toUTCDate())
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else {
+                            return
+                        }
+                        self.section.setAccessory(.none, forRowAt: indexPath.row)
+                        let title = R.string.localizable.restore_from_icloud_confirmation(messageCount, createdAt)
+                        self.alert(title, actionTitle: R.string.localizable.overwrite()) { _ in
+                            self.restoreFromCloud()
+                        }
+                        self.isQuerying = false
+                    }
+                } else {
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else {
+                            return
+                        }
+                        self.isQuerying = false
+                        self.section.setAccessory(.none, forRowAt: indexPath.row)
+                        self.restoreFromCloud()
+                    }
+                }
             }
         } else {
             restoreFromCloud()
         }
     }
+    
 }
 
 extension RestoreFromCloudViewController {

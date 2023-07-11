32ec101e8412193e74f27980482f2b343b8f93aa
wuyueyang <wuyueyang@mixin.one>
2023-07-11 23:47:09 +0800

    Extract base classes

---
+++ Mixin/UserInterface/Views/AssetComboBoxView.swift
@@ -0,0 +1,26 @@
+import UIKit
+import MixinServices
+
+class AssetComboBoxView: ComboBoxView {
+    
+    let iconView = AssetIconView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        insertIconView(iconView)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        insertIconView(iconView)
+    }
+    
+    func load(asset: AssetItem) {
+        titleLabel.text = asset.name
+        let balance = CurrencyFormatter.localizedString(from: asset.balance, format: .precision, sign: .never) ?? asset.localizedBalance
+        subtitleLabel.text = balance + " " + asset.symbol
+        iconView.prepareForReuse()
+        iconView.setIcon(asset: asset)
+    }
+    
+}

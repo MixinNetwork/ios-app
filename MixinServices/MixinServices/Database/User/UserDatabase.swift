88a4236865bb377363b59f906a8a8bcefad22975
wuyueyang <wuyueyang@mixin.one>
2023-10-28 15:16:30 +0800

    Add index

--- MixinServices/MixinServices/Database/User/UserDatabase.swift
+++ MixinServices/MixinServices/Database/User/UserDatabase.swift
@@ -674,7 +674,8 @@ public final class UserDatabase: Database {
                 """,
                 
                 "CREATE INDEX IF NOT EXISTS `index_outputs_asset_state_created_at` ON `outputs` (`asset`, `state`, `created_at`)",
-                "CREATE UNIQUE INDEX IF NOT EXISTS `index_outputs_transaction_hash_output_index` ON `outputs` (`transaction_hash`, `output_index`)"
+                "CREATE UNIQUE INDEX IF NOT EXISTS `index_outputs_transaction_hash_output_index` ON `outputs` (`transaction_hash`, `output_index`)",
+                "CREATE INDEX IF NOT EXISTS `index_tokens_extra_kernel_asset_id` ON `tokens_extra` (`kernel_asset_id`)",
             ]
             for sql in sqls {
                 try db.execute(sql: sql)

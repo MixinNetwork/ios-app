dd1655277fa7f367e7f15eb1280f2bcb5760fe26
wuyueyang <wuyueyang@mixin.one>
2023-11-02 22:57:08 +0800

    Fix token icon

--- Mixin/UserInterface/Controllers/Chat/Views/SnapshotMessageContentView.swift
+++ Mixin/UserInterface/Controllers/Chat/Views/SnapshotMessageContentView.swift
@@ -9,9 +9,13 @@ final class SnapshotMessageContentView: UIView {
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var memoLabel: UILabel!
     
+    @IBOutlet weak var tokenIconWidthConstraint: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         contentStackView.setCustomSpacing(8, after: amountLabel)
+        tokenIconImageView.layer.masksToBounds = true
+        tokenIconImageView.layer.cornerRadius = tokenIconWidthConstraint.constant / 2
     }
     
 }

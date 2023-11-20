c7e3d6ef5d601f3643b683cf3b93830308926e2b
wuyueyang <wuyueyang@mixin.one>
2023-11-20 17:48:23 +0800

    Handle new payment links

---
+++ Mixin/Constants/Payment.swift
@@ -0,0 +1,98 @@
+import Foundation
+
+struct Payment {
+    
+    private static let schemes = ["mixin", "https"]
+    private static let host = "mixin.one"
+    
+    enum Address {
+        case user(String)
+    }
+    
+    let address: Address
+    let asset: String?
+    let amount: Decimal?
+    let memo: String
+    let trace: String
+    let origin: URL?
+    
+    init?(url: URL) {
+        guard let scheme = url.scheme, Self.schemes.contains(scheme) else {
+            return nil
+        }
+        guard url.host == Self.host else {
+            return nil
+        }
+        
+        let pathComponents = url.pathComponents
+        guard pathComponents.count == 3, pathComponents[1] == "pay" else {
+            return nil
+        }
+        
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            return nil
+        }
+        let queries: [String: String]
+        if let items = components.queryItems {
+            queries = items.reduce(into: [:], { result, item in
+                result[item.name] = item.value
+            })
+        } else {
+            queries = [:]
+        }
+        
+        let asset: String?
+        if let id = queries["asset"] {
+            if UUID.isValidLowercasedUUIDString(id) {
+                asset = id
+            } else {
+                return nil
+            }
+        } else {
+            asset = nil
+        }
+        
+        let decimalAmount: Decimal?
+        if let amount = queries["amount"] {
+            if let amount = Decimal(string: amount, locale: .enUSPOSIX), amount > 0 {
+                decimalAmount = amount
+            } else {
+                return nil
+            }
+        } else {
+            decimalAmount = nil
+        }
+        
+        let trace: String
+        if let id = queries["trace"] {
+            if UUID.isValidLowercasedUUIDString(id) {
+                trace = id
+            } else {
+                return nil
+            }
+        } else {
+            trace = UUID().uuidString.lowercased()
+        }
+        
+        let origin: URL?
+        if let returnTo = queries["return_to"] {
+            origin = URL(string: returnTo)
+        } else {
+            origin = nil
+        }
+        
+        let addressString = pathComponents[2]
+        if UUID.isValidLowercasedUUIDString(addressString) {
+            self.address = .user(addressString)
+        } else {
+            return nil
+        }
+        
+        self.asset = asset
+        self.amount = decimalAmount
+        self.memo = queries["memo"] ?? ""
+        self.trace = trace
+        self.origin = origin
+    }
+    
+}
--- Mixin/UserInterface/Windows/UrlWindow.swift
+++ Mixin/UserInterface/Windows/UrlWindow.swift
@@ -17,7 +17,10 @@ class UrlWindow {
         clearNavigationStack: Bool = true,
         presentHintOnUnsupportedMixinSchema: Bool = true
     ) -> Bool {
-        if let mixinURL = MixinURL(url: url) {
+        if let payment = Payment(url: url) {
+            checkPayment(payment)
+            return true
+        } else if let mixinURL = MixinURL(url: url) {
             let result: Bool
             switch mixinURL {
             case let .codes(code):
@@ -851,6 +854,63 @@ class UrlWindow {
 
 extension UrlWindow {
 
+    private static func checkPayment(_ payment: Payment) {
+        let hud = Hud()
+        hud.show(style: .busy, text: "", on: AppDelegate.current.mainWindow)
+        
+        DispatchQueue.global().async {
+            let token: TokenItem?
+            if let asset = payment.asset {
+                token = TokenDAO.shared.tokenItem(with: asset)
+            } else {
+                token = nil
+            }
+            
+            let user: UserItem
+            switch payment.address {
+            case let .user(id):
+                if let (item, _) = syncUser(userId: id, hud: hud) {
+                    user = item
+                } else {
+                    return
+                }
+            }
+            
+            if let token, let amount = payment.amount {
+                let fiatMoneyAmount = amount * token.decimalUSDPrice * Decimal(Currency.current.rate)
+                DispatchQueue.main.async {
+                    let validator = PaymentValidator(traceID: payment.trace, token: token, memo: payment.memo)
+                    validator.transfer(amount: amount, fiatMoneyAmount: fiatMoneyAmount, to: user) { result in
+                        switch result {
+                        case .passed:
+                            hud.hide()
+                            let transfer = TransferConfirmationViewController(opponent: user,
+                                                                              token: token,
+                                                                              amountDisplay: .byToken,
+                                                                              tokenAmount: amount,
+                                                                              fiatMoneyAmount: fiatMoneyAmount,
+                                                                              memo: payment.memo,
+                                                                              traceID: payment.trace)
+                            let authentication = AuthenticationViewController(intentViewController: transfer)
+                            UIApplication.homeContainerViewController?.present(authentication, animated: true, completion: nil)
+                        case .userCancelled:
+                            hud.hide()
+                        case .failure(let message):
+                            hud.set(style: .error, text: message)
+                            hud.scheduleAutoHidden()
+                        }
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    hud.hide()
+                    let transfer = TransferOutViewController.instance(token: token, to: .contact(user))
+                    UIApplication.homeNavigationController?.pushViewController(transfer, animated: true)
+                }
+            }
+        }
+    }
+    
     private static func checkCodesUrl(_ codeId: String, clearNavigationStack: Bool, webContext: MixinWebViewController.Context? = nil) -> Bool {
         guard !codeId.isEmpty, UUID(uuidString: codeId) != nil else {
             return false

43cea233a6b60c348233218d3d9917adf98c522e
wuyueyang <wuyueyang@mixin.one>
2023-11-23 15:27:59 +0800

    Tell user the link is already paid

--- MixinServices/MixinServices/Services/API/Model/TransactionResponse.swift
+++ MixinServices/MixinServices/Services/API/Model/TransactionResponse.swift
@@ -3,14 +3,9 @@ import Foundation
 public struct TransactionResponse {
     
     public let requestID: String
-    public let userID: String
     public let amount: String
     public let transactionHash: String
     public let asset: String
-    public let sendersHash: String
-    public let sendersThreshold: Int
-    public let senders: [String]
-    public let signers: [String]
     public let extra: String
     public let state: String
     public let rawTransaction: String
@@ -26,14 +21,9 @@ extension TransactionResponse: Decodable {
     
     enum CodingKeys: String, CodingKey {
         case requestID = "request_id"
-        case userID = "user_id"
         case amount
         case transactionHash = "transaction_hash"
         case asset
-        case sendersHash = "senders_hash"
-        case sendersThreshold = "senders_threshold"
-        case senders
-        case signers
         case extra
         case state
         case rawTransaction = "raw_transaction"

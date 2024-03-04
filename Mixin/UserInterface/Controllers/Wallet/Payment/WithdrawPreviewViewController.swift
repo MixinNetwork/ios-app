762e3094e1713688460376fd1359ef627cefb569
wuyueyang <wuyueyang@mixin.one>
2024-03-04 20:49:59 +0800

    Show retry button on certain TIP Node errors

--- Mixin/UserInterface/Controllers/Wallet/Payment/WithdrawPreviewViewController.swift
+++ Mixin/UserInterface/Controllers/Wallet/Payment/WithdrawPreviewViewController.swift
@@ -105,7 +105,7 @@ final class WithdrawPreviewViewController: PaymentPreviewViewController {
                                           style: .destructive)
                     tableView.setContentOffset(.zero, animated: true)
                     switch error {
-                    case MixinAPIResponseError.malformedPin, MixinAPIResponseError.incorrectPin:
+                    case MixinAPIResponseError.malformedPin, MixinAPIResponseError.incorrectPin, TIPNode.Error.response(.incorrectPIN), TIPNode.Error.response(.internalServer):
                         loadDoubleButtonTrayView(leftTitle: R.string.localizable.cancel(),
                                                  leftAction: #selector(close(_:)),
                                                  rightTitle: R.string.localizable.retry(),

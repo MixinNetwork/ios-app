18cf403650f9cce41786e9403cc4c3eb948fbc3c
wuyueyang <wuyueyang@mixin.one>
2024-03-13 12:54:41 +0800

    Refactor explore

---
+++ Mixin/UserInterface/Controllers/Home/Cells/ExploreSegmentCell.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+final class ExploreSegmentCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: InsetLabel!
+    
+    override var isSelected: Bool {
+        didSet {
+            updateColors(isSelected: isSelected)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        label.contentInset = UIEdgeInsets(top: 10, left: 14, bottom: 10, right: 14)
+        label.layer.borderWidth = 1
+        label.layer.cornerRadius = 19
+        label.layer.masksToBounds = true
+        updateColors(isSelected: false)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            updateColors(isSelected: isSelected)
+        }
+    }
+    
+    private func updateColors(isSelected: Bool) {
+        if isSelected {
+            label.layer.borderColor = R.color.theme()!.cgColor
+            label.textColor = R.color.theme()
+        } else {
+            label.layer.borderColor = R.color.line()!.cgColor
+            label.textColor = R.color.text()
+        }
+    }
+    
+}

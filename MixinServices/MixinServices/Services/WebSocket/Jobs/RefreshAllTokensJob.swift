14055c08f164d49c30a01d931ef9b2a28fbed527
wuyueyang <wuyueyang@mixin.one>
2024-04-25 19:02:43 +0800

    Fix new default tokens not showing

--- MixinServices/MixinServices/Services/WebSocket/Jobs/RefreshAllTokensJob.swift
+++ MixinServices/MixinServices/Services/WebSocket/Jobs/RefreshAllTokensJob.swift
@@ -9,16 +9,22 @@ public final class RefreshAllTokensJob: AsynchronousJob {
     public override func execute() -> Bool {
         Task {
             do {
-                let ids = TokenDAO.shared.allAssetIDs()
-                let tokens: [Token]
-                if ids.isEmpty {
-                    tokens = try await SafeAPI.assets()
-                } else {
-                    tokens = try await SafeAPI.assets(ids: ids)
-                }
+                // Save default tokens, currently are native tokens of supported chains
+                let tokens = try await SafeAPI.assets()
                 if !MixinService.isStopProcessMessages {
                     TokenDAO.shared.save(assets: tokens)
                 }
+                
+                // Save existed tokens, includes all tokens displayed in wallet view
+                let ids = TokenDAO.shared.allAssetIDs()
+                if !ids.isEmpty {
+                    let tokens = try await SafeAPI.assets(ids: ids)
+                    if !MixinService.isStopProcessMessages {
+                        TokenDAO.shared.save(assets: tokens)
+                    }
+                }
+                
+                // Save chains
                 let chains = try await NetworkAPI.chains()
                 if !MixinService.isStopProcessMessages {
                     ChainDAO.shared.save(chains)

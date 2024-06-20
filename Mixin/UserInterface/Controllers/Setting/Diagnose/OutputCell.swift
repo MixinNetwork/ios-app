e8822d1a2d74d80da2d3032c1568f55968b71155
wuyueyang <wuyueyang@mixin.one>
2024-06-20 17:15:47 +0800

    Better diagnostic

--- Mixin/UserInterface/Controllers/Setting/Diagnose/OutputCell.swift
+++ Mixin/UserInterface/Controllers/Setting/Diagnose/OutputCell.swift
@@ -3,23 +3,50 @@ import MixinServices
 
 final class OutputCell: UITableViewCell {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var hashLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     
+    private let descriptionAttributes: [NSAttributedString.Key : Any] = [
+        .font: UIFont.monospacedSystemFont(ofSize: 13, weight: .regular),
+        .foregroundColor: R.color.text_tertiary()!
+    ]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        titleLabel.font = .monospacedSystemFont(ofSize: 15, weight: .regular)
+        idLabel.font = .monospacedSystemFont(ofSize: 15, weight: .regular)
+        hashLabel.font = .monospacedSystemFont(ofSize: 13, weight: .regular)
         descriptionLabel.font = .monospacedSystemFont(ofSize: 13, weight: .regular)
     }
     
     func load(output: Output, showAssetID: Bool) {
-        titleLabel.text = output.id
-        if showAssetID {
-            descriptionLabel.text = "\(output.sequence):\(output.state):\(output.asset)"
-        } else {
-            descriptionLabel.text = "\(output.sequence):\(output.state)"
-        }
+        idLabel.text = output.id
+        hashLabel.text = output.transactionHash
+        descriptionLabel.attributedText = {
+            let description = NSMutableAttributedString(string: "\(output.sequence):", attributes: descriptionAttributes)
+            
+            let stateColor: UIColor
+            switch output.state {
+            case "signed":
+                stateColor = R.color.red()!
+            case "unspent":
+                stateColor = R.color.green()!
+            default:
+                stateColor = R.color.text_tertiary()!
+            }
+            var stateAttributes = descriptionAttributes
+            stateAttributes[.foregroundColor] = stateColor
+            let state = NSMutableAttributedString(string: output.state, attributes: stateAttributes)
+            description.append(state)
+            
+            if showAssetID {
+                let assetID = NSAttributedString(string: ":\(output.asset)", attributes: descriptionAttributes)
+                description.append(assetID)
+            }
+            
+            return description
+        }()
         amountLabel.text = output.amount
     }
     

f846e4057921ef5669d0503b87d90e1b2bd83ff3
wuyueyang <wuyueyang@mixin.one>
2024-07-10 22:42:19 +0800

    Validate ed25519 private key

--- MixinServices/MixinServices/Crypto/Ed25519PrivateKey.swift
+++ MixinServices/MixinServices/Crypto/Ed25519PrivateKey.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CryptoKit
+import TIP
 
 public class Ed25519PrivateKey {
     
@@ -10,12 +11,21 @@ public class Ed25519PrivateKey {
     private let key: Curve25519.Signing.PrivateKey
     
     public convenience init() {
-        let key = Curve25519.Signing.PrivateKey()
+        var key = Curve25519.Signing.PrivateKey()
+        var goImpl = Ed25519NewKeyFromSeed(key.rawRepresentation)
+        while goImpl == nil {
+            key = Curve25519.Signing.PrivateKey()
+            goImpl = Ed25519NewKeyFromSeed(key.rawRepresentation)
+        }
         self.init(key: key)
     }
     
     public convenience init(rawRepresentation: Data) throws {
         let key = try Curve25519.Signing.PrivateKey(rawRepresentation: rawRepresentation)
+        let goImpl = Ed25519NewKeyFromSeed(key.rawRepresentation)
+        guard goImpl != nil else {
+            throw ValidationError.invalidSeed
+        }
         self.init(key: key)
     }
     
@@ -34,6 +44,10 @@ public class Ed25519PrivateKey {
 }
 
 extension Ed25519PrivateKey {
+    
+    enum ValidationError: Error {
+        case invalidSeed
+    }
     
     private static func x25519(from ed25519: Data) -> Data {
         let hash = SHA512.hash(data: ed25519)

c36564bd5f36cfe696ad0273176b3a84fb96c954
wuyueyang <wuyueyang@mixin.one>
2024-09-19 16:21:11 +0800

    Update dependencies

--- Mixin/Service/PhoneNumberValidator.swift
+++ Mixin/Service/PhoneNumberValidator.swift
@@ -5,21 +5,21 @@ final class PhoneNumberValidator {
     
     static let global = PhoneNumberValidator()
     
-    let kit = PhoneNumberKit()
+    let utility = PhoneNumberUtility()
     
     private init() {
         
     }
     
     func isValid(callingCode: String, number: String) -> Bool {
-        if (try? kit.parse("+" + callingCode + number)) != nil {
+        if (try? utility.parse("+" + callingCode + number)) != nil {
             return true
         } else if callingCode == "225" {
             // Côte d'Ivoire
             let possiblePrefixes = ["07", "05", "01", "27", "25", "21"]
             return possiblePrefixes.contains(where: { prefix in
                 let possibleNumber = "+" + callingCode + prefix + number
-                return (try? kit.parse(possibleNumber)) != nil
+                return (try? utility.parse(possibleNumber)) != nil
             })
         } else {
             return false
@@ -27,7 +27,7 @@ final class PhoneNumberValidator {
     }
     
     func isValid(_ number: String) -> Bool {
-        if (try? kit.parse(number)) != nil {
+        if (try? utility.parse(number)) != nil {
             return true
         } else if number.hasPrefix("+225"), number.count == 12 {
             // Côte d'Ivoire
@@ -36,7 +36,7 @@ final class PhoneNumberValidator {
                 let mobileNumberStart = number.index(number.startIndex, offsetBy: 4)
                 let mobileNumberEnd = number.endIndex
                 let possibleNumber = "+225" + prefix + number[mobileNumberStart..<mobileNumberEnd]
-                return (try? kit.parse(possibleNumber)) != nil
+                return (try? utility.parse(possibleNumber)) != nil
             })
         } else {
             return false

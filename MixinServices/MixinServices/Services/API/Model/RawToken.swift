38537cdefacd275c43f904904b5520c408ccbad0
wuyueyang <wuyueyang@mixin.one>
2024-10-08 21:31:34 +0800

    Clarify property name

--- MixinServices/MixinServices/Services/API/Model/RawToken.swift
+++ MixinServices/MixinServices/Services/API/Model/RawToken.swift
@@ -10,8 +10,8 @@ struct RawToken {
     
     let assetID: String
     let kernelAssetID: String
-    let symbol: String
-    let name: String
+    let displaySymbol: String
+    let displayName: String
     let iconURL: String
     let btcPrice: String
     let usdPrice: String
@@ -27,8 +27,8 @@ struct RawToken {
         Token(
             assetID: assetID,
             kernelAssetID: kernelAssetID,
-            symbol: symbol,
-            name: name,
+            symbol: displaySymbol,
+            name: displayName,
             iconURL: iconURL,
             btcPrice: btcPrice,
             usdPrice: usdPrice,
@@ -49,8 +49,8 @@ extension RawToken: Decodable {
     enum CodingKeys: String, CodingKey {
         case assetID = "asset_id"
         case kernelAssetID = "kernel_asset_id"
-        case symbol = "display_symbol"
-        case name = "display_name"
+        case displaySymbol = "display_symbol"
+        case displayName = "display_name"
         case iconURL = "icon_url"
         case btcPrice = "price_btc"
         case usdPrice = "price_usd"

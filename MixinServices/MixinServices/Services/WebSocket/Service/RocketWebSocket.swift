e23904faf5cb17658f7b64b7b65d46104c6589f8
wuyueyang <wuyueyang@mixin.one>
2024-10-20 23:19:44 +0800

    Fix host auto-switching

--- MixinServices/MixinServices/Services/WebSocket/Service/RocketWebSocket.swift
+++ MixinServices/MixinServices/Services/WebSocket/Service/RocketWebSocket.swift
@@ -71,7 +71,10 @@ extension RocketWebSocket: SRWebSocketDelegate {
         Logger.general.error(category: "RocketWebSocket", message: "Websocket failed with: \(err), host: \(MixinHost.webSocket)")
 
         if (nsError.domain == "com.squareup.SocketRocket" && nsError.code == 504)
-            || (nsError.domain == NSPOSIXErrorDomain && nsError.code == 61) || nsError.domain == SRWebSocketErrorDomain {
+            || (nsError.domain == NSPOSIXErrorDomain && nsError.code == 61)
+            || nsError.domain == SRWebSocketErrorDomain
+            || (nsError.domain == NSURLErrorDomain && nsError.code == NSURLErrorTimedOut)
+        {
             // Connection time out or refused
             delegate?.websocketDidDisconnect(socket: self, isSwitchNetwork: true)
         } else {

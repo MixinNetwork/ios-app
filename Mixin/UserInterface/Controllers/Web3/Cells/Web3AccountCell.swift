f4d9dca89bc387eeb944e6e63fb78ca562bef0b0
wuyueyang <wuyueyang@mixin.one>
2025-02-12 15:25:50 +0800

    Remove file not in used

--- Mixin/UserInterface/Controllers/Web3/Cells/Web3AccountCell.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-final class Web3AccountCell: ModernSelectedBackgroundCell {
-    
-    @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
-    
-}

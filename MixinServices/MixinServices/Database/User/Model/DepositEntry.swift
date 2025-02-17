954bed74a056d052ca40784dd00ece5e967bb31a
wuyueyang <wuyueyang@mixin.one>
2025-02-17 17:36:27 +0800

    Verify deposit entries with designated keys

--- MixinServices/MixinServices/Database/User/Model/DepositEntry.swift
+++ MixinServices/MixinServices/Database/User/Model/DepositEntry.swift
@@ -38,9 +38,15 @@ extension DepositEntry: TableRecord, PersistableRecord {
 
 extension DepositEntry {
     
-    private static let kernelPublicKey: Curve25519.Signing.PublicKey = {
-        let data = Data(hexEncodedString: "8f94e89d03fa128a7081c5fe73c6814010c5ca74438411a42df87c6023dfa94d")!
-        return try! Curve25519.Signing.PublicKey(rawRepresentation: data)
+    private static let kernelPublicKeys: [Curve25519.Signing.PublicKey] = {
+        let keys = [
+            "8f94e89d03fa128a7081c5fe73c6814010c5ca74438411a42df87c6023dfa94d",
+            "2dc073588908a02284197ad78fc863e83c760dabcd5d9a508e09a799ebc1ecb8",
+        ]
+        return keys.map { key in
+            let data = Data(hexEncodedString: key)!
+            return try! Curve25519.Signing.PublicKey(rawRepresentation: data)
+        }
     }()
     
     public var isSignatureValid: Bool {
@@ -56,7 +62,9 @@ extension DepositEntry {
         guard let content = content.data(using: .utf8), let contentHash = SHA3_256.hash(data: content) else {
             return false
         }
-        return Self.kernelPublicKey.isValidSignature(signature, for: contentHash)
+        return Self.kernelPublicKeys.contains { key in
+            key.isValidSignature(signature, for: contentHash)
+        }
     }
     
 }

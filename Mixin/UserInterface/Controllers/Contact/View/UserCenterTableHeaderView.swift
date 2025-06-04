60a8dc15f274b37a76966e7d19256d86d031dde6
wuyueyang <wuyueyang@mixin.one>
2025-06-04 21:58:40 +0800

    Buy membership by tapping icon in user center

--- Mixin/UserInterface/Controllers/Contact/View/UserCenterTableHeaderView.swift
+++ Mixin/UserInterface/Controllers/Contact/View/UserCenterTableHeaderView.swift
@@ -1,11 +1,33 @@
 import UIKit
 import SDWebImage
 
-class UserCenterTableHeaderView: UIView {
+final class UserCenterTableHeaderView: UIView {
     
     @IBOutlet weak var avatarImageView: AvatarImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var membershipImageView: SDAnimatedImageView!
     @IBOutlet weak var identityNumberLabel: IdentityNumberLabel!
-            
+    
+    private weak var membershipButton: UIButton?
+    
+    func addMembershipButton(target: Any, action: Selector) {
+        if let button = membershipButton {
+            button.removeTarget(nil, action: nil, for: .allEvents)
+            button.addTarget(target, action: action, for: .touchUpInside)
+        } else {
+            let button = UIButton()
+            button.addTarget(target, action: action, for: .touchUpInside)
+            addSubview(button)
+            button.snp.makeConstraints { make in
+                make.width.height.equalTo(30)
+                make.center.equalTo(membershipImageView)
+            }
+            membershipButton = button
+        }
+    }
+    
+    func removeMembershipButton() {
+        membershipButton?.removeFromSuperview()
+    }
+    
 }

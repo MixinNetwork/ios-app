47e3c1a1b459a38e058efd73f3abb14df4773f01
wuyueyang <wuyueyang@mixin.one>
2025-07-23 15:58:55 +0800

    Extract AddWalletInputOnChainInfoViewController

---
+++ Mixin/UserInterface/Controllers/Wallet/Import/AddWalletInputOnChainInfoViewController.swift
@@ -0,0 +1,189 @@
+import UIKit
+import MixinServices
+
+class AddWalletInputOnChainInfoViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var networkSelectorBackgroundView: UIView!
+    @IBOutlet weak var networkTitleLabel: UILabel!
+    @IBOutlet weak var networkNameLabel: UILabel!
+    @IBOutlet weak var selectNetworkButton: MenuTriggerButton!
+    @IBOutlet weak var inputBackgroundView: UIView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputPlaceholderLabel: InsetLabel!
+    @IBOutlet weak var deleteInputButton: UIButton!
+    @IBOutlet weak var pasteInputButton: UIButton!
+    @IBOutlet weak var scanInputButton: UIButton!
+    @IBOutlet weak var descriptionLabel: InsetLabel!
+    @IBOutlet weak var errorDescriptionLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    private weak var contentHeightConstraint: NSLayoutConstraint!
+    
+    private(set) var selectedChain: Web3Chain = .ethereum {
+        didSet {
+            reloadViews(chain: selectedChain)
+            detectInput()
+        }
+    }
+    
+    init() {
+        let nib = R.nib.addWalletInputOnChainInfoView
+        super.init(nibName: nib.name, bundle: nib.bundle)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Storyboard not supported")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = .customerService(
+            target: self,
+            action: #selector(presentCustomerService(_:))
+        )
+        
+        let contentLayoutGuide = UILayoutGuide()
+        view.addLayoutGuide(contentLayoutGuide)
+        contentLayoutGuide.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.keyboardLayoutGuide.snp.top)
+        }
+        contentHeightConstraint = contentView.heightAnchor.constraint(equalTo: contentLayoutGuide.heightAnchor, multiplier: 1)
+        contentHeightConstraint.isActive = true
+        
+        for view: UIView in [networkSelectorBackgroundView, inputBackgroundView] {
+            view.layer.cornerRadius = 8
+            view.layer.masksToBounds = true
+        }
+        networkTitleLabel.text = R.string.localizable.network()
+        selectNetworkButton.showsMenuAsPrimaryAction = true
+        reloadViews(chain: selectedChain)
+        inputTextView.textContainerInset = .zero
+        inputTextView.textContainer.lineFragmentPadding = 0
+        inputTextView.font = UIFontMetrics.default.scaledFont(
+            for: .monospacedSystemFont(ofSize: 16, weight: .regular)
+        )
+        descriptionLabel.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        descriptionLabel.setFont(scaledFor: .systemFont(ofSize: 14), adjustForContentSize: true)
+        errorDescriptionLabel.setFont(scaledFor: .systemFont(ofSize: 14), adjustForContentSize: true)
+        continueButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(rearrangeInputButtons(_:)),
+            name: UITextView.textDidChangeNotification,
+            object: inputTextView
+        )
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(layoutContentByKeyboard(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(layoutContentByKeyboard(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @IBAction func deleteInput(_ sender: Any) {
+        inputTextView.text = ""
+        rearrangeInputButtons(sender)
+        detectInput()
+    }
+    
+    @IBAction func pasteInput(_ sender: Any) {
+        inputTextView.text = UIPasteboard.general.string
+        rearrangeInputButtons(sender)
+        detectInput()
+    }
+    
+    @IBAction func scanInput(_ sender: Any) {
+        let scanner = CameraViewController.instance()
+        scanner.asQrCodeScanner = true
+        scanner.delegate = self
+        navigationController?.pushViewController(scanner, animated: true)
+    }
+    
+    @IBAction func continueToNext(_ sender: Any) {
+        
+    }
+    
+    func detectInput() {
+        
+    }
+    
+    @objc private func presentCustomerService(_ sender: Any) {
+        let customerService = CustomerServiceViewController()
+        present(customerService, animated: true)
+        reporter.report(event: .customerServiceDialog, tags: ["source": "input_private_key"])
+    }
+    
+    @objc private func rearrangeInputButtons(_ sender: Any) {
+        if inputTextView.text.isEmpty {
+            deleteInputButton.isHidden = true
+            pasteInputButton.isHidden = false
+            scanInputButton.isHidden = false
+        } else {
+            deleteInputButton.isHidden = false
+            pasteInputButton.isHidden = true
+            scanInputButton.isHidden = true
+        }
+    }
+    
+    @objc private func layoutContentByKeyboard(_ notification: Notification) {
+        switch notification.name {
+        case UIResponder.keyboardWillShowNotification:
+            contentHeightConstraint.priority = .defaultHigh
+        case UIResponder.keyboardWillHideNotification:
+            contentHeightConstraint.priority = .defaultLow
+        default:
+            return
+        }
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: .overdampedCurve,
+            animations: view.layoutIfNeeded
+        )
+    }
+    
+    private func reloadViews(chain: Web3Chain) {
+        networkNameLabel.text = chain.name
+        selectNetworkButton.menu = UIMenu(children: Web3Chain.all.map { chain in
+            UIAction(
+                title: chain.name,
+                state: chain == selectedChain ? .on : .off,
+                handler: { [weak self] _ in self?.selectedChain = chain }
+            )
+        })
+    }
+    
+}
+
+extension AddWalletInputOnChainInfoViewController: HomeNavigationController.NavigationBarStyling {
+    
+    var navigationBarStyle: HomeNavigationController.NavigationBarStyle {
+        .secondaryBackground
+    }
+    
+}
+
+extension AddWalletInputOnChainInfoViewController: CameraViewControllerDelegate {
+    
+    func cameraViewController(_ controller: CameraViewController, shouldRecognizeString string: String) -> Bool {
+        inputTextView.text = string
+        rearrangeInputButtons(controller)
+        detectInput()
+        return false
+    }
+    
+}
--- Mixin/UserInterface/Controllers/Wallet/Import/AddWalletInputPrivateKeyViewController.swift
+++ Mixin/UserInterface/Controllers/Wallet/Import/AddWalletInputPrivateKeyViewController.swift
@@ -2,192 +2,7 @@ import UIKit
 import web3
 import MixinServices
 
-final class AddWalletInputPrivateKeyViewController: UIViewController {
-    
-    @IBOutlet weak var contentView: UIView!
-    @IBOutlet weak var networkSelectorBackgroundView: UIView!
-    @IBOutlet weak var networkTitleLabel: UILabel!
-    @IBOutlet weak var networkNameLabel: UILabel!
-    @IBOutlet weak var selectNetworkButton: MenuTriggerButton!
-    @IBOutlet weak var privateKeyBackgroundView: UIView!
-    @IBOutlet weak var privateKeyTextView: UITextView!
-    @IBOutlet weak var privateKeyPlaceholderLabel: InsetLabel!
-    @IBOutlet weak var descriptionLabel: InsetLabel!
-    @IBOutlet weak var errorDescriptionLabel: UILabel!
-    @IBOutlet weak var importButton: UIButton!
-    
-    private let encryptionKey: Data
-    
-    private weak var contentHeightConstraint: NSLayoutConstraint!
-    
-    private var selectedChain: Web3Chain = .solana {
-        didSet {
-            reloadViews(chain: selectedChain)
-            detectInput()
-        }
-    }
-    
-    private var wallet: Wallet? {
-        didSet {
-            importButton.isEnabled = wallet != nil
-        }
-    }
-    
-    init(encryptionKey: Data) {
-        self.encryptionKey = encryptionKey
-        let nib = R.nib.addWalletInputPrivateKeyView
-        super.init(nibName: nib.name, bundle: nib.bundle)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("Storyboard not supported")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = R.string.localizable.import_private_key()
-        navigationItem.rightBarButtonItem = .customerService(
-            target: self,
-            action: #selector(presentCustomerService(_:))
-        )
-        
-        let contentLayoutGuide = UILayoutGuide()
-        view.addLayoutGuide(contentLayoutGuide)
-        contentLayoutGuide.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.leading.trailing.equalToSuperview()
-            make.bottom.equalTo(view.keyboardLayoutGuide.snp.top)
-        }
-        contentHeightConstraint = contentView.heightAnchor.constraint(equalTo: contentLayoutGuide.heightAnchor, multiplier: 1)
-        contentHeightConstraint.isActive = true
-        
-        for view: UIView in [networkSelectorBackgroundView, privateKeyBackgroundView] {
-            view.layer.cornerRadius = 8
-            view.layer.masksToBounds = true
-        }
-        networkTitleLabel.text = R.string.localizable.network()
-        selectNetworkButton.showsMenuAsPrimaryAction = true
-        reloadViews(chain: selectedChain)
-        privateKeyTextView.delegate = self
-        privateKeyTextView.textContainerInset = .zero
-        privateKeyTextView.textContainer.lineFragmentPadding = 0
-        privateKeyTextView.font = UIFontMetrics.default.scaledFont(
-            for: .monospacedSystemFont(ofSize: 16, weight: .regular)
-        )
-        privateKeyPlaceholderLabel.text = R.string.localizable.type_your_private_key()
-        descriptionLabel.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        descriptionLabel.setFont(scaledFor: .systemFont(ofSize: 14), adjustForContentSize: true)
-        descriptionLabel.text = R.string.localizable.private_key_storage_description()
-        errorDescriptionLabel.setFont(scaledFor: .systemFont(ofSize: 14), adjustForContentSize: true)
-        importButton.configuration?.title = R.string.localizable.import()
-        importButton.titleLabel?.adjustsFontForContentSizeCategory = true
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(layoutContentByKeyboard(_:)),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(layoutContentByKeyboard(_:)),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
-    }
-    
-    @IBAction func pastePrivateKey(_ sender: Any) {
-        privateKeyTextView.text = UIPasteboard.general.string
-        detectInput()
-    }
-    
-    @IBAction func scanPrivateKey(_ sender: Any) {
-        let scanner = CameraViewController.instance()
-        scanner.asQrCodeScanner = true
-        scanner.delegate = self
-        navigationController?.pushViewController(scanner, animated: true)
-    }
-    
-    @IBAction func importWallet(_ sender: Any) {
-        guard let wallet else {
-            return
-        }
-        let importing = AddWalletImportingViewController(
-            importingWallet: .byPrivateKey(
-                key: wallet.privateKey,
-                address: wallet.address,
-                chainID: wallet.chainID
-            )
-        )
-        navigationController?.pushViewController(importing, animated: true)
-    }
-    
-    @objc private func presentCustomerService(_ sender: Any) {
-        let customerService = CustomerServiceViewController()
-        present(customerService, animated: true)
-        reporter.report(event: .customerServiceDialog, tags: ["source": "input_private_key"])
-    }
-    
-    @objc private func layoutContentByKeyboard(_ notification: Notification) {
-        switch notification.name {
-        case UIResponder.keyboardWillShowNotification:
-            contentHeightConstraint.priority = .defaultHigh
-        case UIResponder.keyboardWillHideNotification:
-            contentHeightConstraint.priority = .defaultLow
-        default:
-            return
-        }
-        UIView.animate(
-            withDuration: 0.5,
-            delay: 0,
-            options: .overdampedCurve,
-            animations: view.layoutIfNeeded
-        )
-    }
-    
-}
-
-extension AddWalletInputPrivateKeyViewController: HomeNavigationController.NavigationBarStyling {
-    
-    var navigationBarStyle: HomeNavigationController.NavigationBarStyle {
-        .secondaryBackground
-    }
-    
-}
-
-extension AddWalletInputPrivateKeyViewController: UITextViewDelegate {
-    
-    func textViewDidChange(_ textView: UITextView) {
-        detectInput()
-    }
-    
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder()
-            if wallet == nil {
-                errorDescriptionLabel.text = R.string.localizable.invalid_format()
-            } else {
-                errorDescriptionLabel.text = nil
-            }
-            return false
-        } else {
-            return true
-        }
-    }
-    
-}
-
-extension AddWalletInputPrivateKeyViewController: CameraViewControllerDelegate {
-    
-    func cameraViewController(_ controller: CameraViewController, shouldRecognizeString string: String) -> Bool {
-        privateKeyTextView.text = string
-        detectInput()
-        return false
-    }
-    
-}
-
-extension AddWalletInputPrivateKeyViewController {
+final class AddWalletInputPrivateKeyViewController: AddWalletInputOnChainInfoViewController {
     
     private enum LoadKeyError: Error {
         case invalidHex
@@ -202,27 +17,56 @@ extension AddWalletInputPrivateKeyViewController {
         let chainID: String
     }
     
-    private func reloadViews(chain: Web3Chain) {
-        networkNameLabel.text = chain.name
-        selectNetworkButton.menu = UIMenu(children: Web3Chain.all.map { chain in
-            UIAction(
-                title: chain.name,
-                state: chain == selectedChain ? .on : .off,
-                handler: { [weak self] _ in self?.selectedChain = chain }
-            )
-        })
+    private let encryptionKey: Data
+    
+    private var wallet: Wallet? {
+        didSet {
+            continueButton.isEnabled = wallet != nil
+        }
     }
     
-    private func detectInput() {
+    init(encryptionKey: Data) {
+        self.encryptionKey = encryptionKey
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Storyboard not supported")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = R.string.localizable.import_private_key()
+        inputTextView.delegate = self
+        inputPlaceholderLabel.text = R.string.localizable.type_your_private_key()
+        descriptionLabel.text = R.string.localizable.private_key_storage_description()
+        continueButton.configuration?.title = R.string.localizable.import()
+    }
+    
+    override func continueToNext(_ sender: Any) {
+        guard let wallet else {
+            return
+        }
+        let importing = AddWalletImportingViewController(
+            importingWallet: .byPrivateKey(
+                key: wallet.privateKey,
+                address: wallet.address,
+                chainID: wallet.chainID
+            )
+        )
+        navigationController?.pushViewController(importing, animated: true)
+    }
+    
+    override func detectInput() {
         errorDescriptionLabel.text = nil
-        let input = (privateKeyTextView.text ?? "")
+        let input = (inputTextView.text ?? "")
             .trimmingCharacters(in: .whitespacesAndNewlines)
         guard !input.isEmpty else {
-            privateKeyPlaceholderLabel.isHidden = false
+            inputPlaceholderLabel.isHidden = false
             wallet = nil
             return
         }
-        privateKeyPlaceholderLabel.isHidden = true
+        inputPlaceholderLabel.isHidden = true
         do {
             switch selectedChain.kind {
             case .evm:
@@ -272,6 +116,28 @@ extension AddWalletInputPrivateKeyViewController {
         } catch {
             Logger.general.debug(category: "InputPrivateKey", message: "\(error)")
             wallet = nil
+        }
+    }
+    
+}
+
+extension AddWalletInputPrivateKeyViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        detectInput()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            if wallet == nil {
+                errorDescriptionLabel.text = R.string.localizable.invalid_format()
+            } else {
+                errorDescriptionLabel.text = nil
+            }
+            return false
+        } else {
+            return true
         }
     }
     

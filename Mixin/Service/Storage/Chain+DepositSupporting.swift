de83ba6b35394cca26e2f12e3aae4b90a1ccf79f
wuyueyang <wuyueyang@mixin.one>
2025-09-12 16:41:22 +0800

    Update deposit supporting description for TON network

--- Mixin/Service/Storage/Chain+DepositSupporting.swift
+++ Mixin/Service/Storage/Chain+DepositSupporting.swift
@@ -5,7 +5,8 @@ extension Chain {
     
     var depositSupporting: String {
         switch chainId {
-        case ChainID.eos, ChainID.solana, ChainID.bnbSmartChain, ChainID.base, ChainID.ripple, ChainID.polygon, ChainID.mobilecoin:
+        case ChainID.eos, ChainID.solana, ChainID.bnbSmartChain, ChainID.base,
+            ChainID.ripple, ChainID.polygon, ChainID.mobilecoin, ChainID.ton:
             R.string.localizable.deposit_supporting_token_of_network(name)
         case ChainID.ethereum:
             R.string.localizable.deposit_tip_eth()

e7a72da5d3268887a7507bcc714e4352e254495b
wuyueyang <wuyueyang@mixin.one>
2025-09-23 19:25:36 +0800

    Log TIP public key existence

--- Mixin/UserInterface/Controllers/Login/Model/LoginAccountHandler.swift
+++ Mixin/UserInterface/Controllers/Login/Model/LoginAccountHandler.swift
@@ -20,7 +20,7 @@ extension LoginAccountHandler where Self: UIViewController {
             Logger.login.error(category: "Login", message: "Invalid Server PIN Token")
             return .invalidServerPinToken
         }
-        Logger.login.info(category: "Login", message: "Got account: \(account.userID), has_pin: \(account.hasPIN), has_safe: \(account.hasSafe)")
+        Logger.login.info(category: "Login", message: "Got account: \(account.userID), has_pin: \(account.hasPIN), has_safe: \(account.hasSafe), tip_key: \(account.tipKey?.count ?? -1)")
         AppGroupKeychain.sessionSecret = sessionKey.rawRepresentation
         AppGroupKeychain.pinToken = pinToken
         if !account.isAnonymous {

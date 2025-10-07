354a8c6e3bd2d5f270857518e83601b4a7e9a9e8
wuyueyang <wuyueyang@mixin.one>
2025-10-08 00:43:40 +0800

    Refactor swap token selector

---
+++ Mixin/UserInterface/Controllers/Wallet/Swap/SwapMixinTokenSelectorViewController.swift
@@ -0,0 +1,45 @@
+import UIKit
+import OrderedCollections
+import MixinServices
+
+final class SwapMixinTokenSelectorViewController: SwapTokenSelectorViewController {
+    
+    init(
+        recent: Recent,
+        tokens: [BalancedSwapToken],
+        selectedAssetID: String?
+    ) {
+        super.init(
+            recent: recent,
+            supportedChainIDs: nil,
+            searchSource: .mixin,
+            tokens: tokens,
+            selectedAssetID: selectedAssetID
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Storyboard is not supported")
+    }
+    
+    override class func chains(with ids: Set<String>) -> OrderedSet<Chain> {
+        Chain.mixinChains(ids: ids)
+    }
+    
+    override func fillBalance(to tokens: [SwapToken]) -> [BalancedSwapToken] {
+        let ids = tokens.map(\.assetID)
+        let tokenItems = TokenDAO.shared
+            .tokenItems(with: ids)
+            .reduce(into: [:]) { result, item in
+                result[item.assetID] = item
+            }
+        return tokens.map { token in
+            if let item = tokenItems[token.assetID] {
+                BalancedSwapToken(token: token, balance: item.decimalBalance, usdPrice: item.decimalUSDPrice)
+            } else {
+                BalancedSwapToken(token: token, balance: 0, usdPrice: 0)
+            }
+        }
+    }
+    
+}

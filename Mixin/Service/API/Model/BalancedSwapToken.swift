2a6fe8ef3f577c7e580ffebb2fdee8458c46b1cf
wuyueyang <wuyueyang@mixin.one>
2025-10-08 00:43:40 +0800

    Refactor swap token selector

--- Mixin/Service/API/Model/BalancedSwapToken.swift
+++ Mixin/Service/API/Model/BalancedSwapToken.swift
@@ -49,34 +49,3 @@ final class BalancedSwapToken: SwapToken, ValuableToken {
     }
     
 }
-
-extension BalancedSwapToken {
-    
-    static func fillMixinBalance(swappableTokens: [SwapToken]) -> [BalancedSwapToken] {
-        fillBalance(swappableTokens: swappableTokens, walletID: nil)
-    }
-    
-    static func fillWeb3Balance(swappableTokens: [SwapToken], walletID: String) -> [BalancedSwapToken] {
-        fillBalance(swappableTokens: swappableTokens, walletID: walletID)
-    }
-
-    private static func fillBalance(swappableTokens: [SwapToken], walletID: String?) -> [BalancedSwapToken] {
-        let ids = swappableTokens.map(\.assetID)
-        let tokenItems: [ValuableToken] = if let walletID {
-            Web3TokenDAO.shared.tokens(walletID: walletID, ids: ids)
-        } else {
-            TokenDAO.shared.tokenItems(with: ids)
-        }
-        let tokenMaps = tokenItems.reduce(into: [:]) { result, item in
-            result[item.assetID] = item
-        }
-        return swappableTokens.map { token in
-            if let item = tokenMaps[token.assetID] {
-                BalancedSwapToken(token: token, balance: item.decimalBalance, usdPrice: item.decimalUSDPrice)
-            } else {
-                BalancedSwapToken(token: token, balance: 0, usdPrice: 0)
-            }
-        }
-    }
-    
-}

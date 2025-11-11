5e52381a18ad419e42bc7a4510ed9d8d6c4d4068
wuyueyang <wuyueyang@mixin.one>
2025-11-11 14:57:52 +0800

    Fix app crash

--- Mixin/UserInterface/Controllers/Home/ExploreBotsViewController.swift
+++ Mixin/UserInterface/Controllers/Home/ExploreBotsViewController.swift
@@ -197,7 +197,7 @@ final class ExploreBotsViewController: UITableViewController {
                 self.indexedUsers = indexedUsers
                 self.tableView.performBatchUpdates {
                     var lastSectionIndex = self.numberOfSections(in: self.tableView) - 1
-                    if lastSectionIndex > 2 {
+                    if lastSectionIndex >= 2 {
                         self.indexTitles = nil
                         self.tableView.deleteSections(IndexSet(integersIn: 2...lastSectionIndex), with: .none)
                     }

4659df8e4ec57cdfec83d8d684e197a4ff16b83d
wuyueyang <wuyueyang@mixin.one>
2026-08-18 20:55:40 +0800

    Keep the barrier while ignoring the error

--- MixinServices/MixinServices/Database/User/UserDatabase.swift
+++ MixinServices/MixinServices/Database/User/UserDatabase.swift
@@ -1043,7 +1043,7 @@ extension UserDatabase {
 extension UserDatabase {
     
     public func clearSentSenderKey(sessionID: String) {
-        write { (db) -> Void in
+        try? pool.barrierWriteWithoutTransaction { (db) -> Void in
             try db.execute(sql: "UPDATE participant_session SET sent_to_server = NULL")
             try db.execute(sql: "DELETE FROM participant_session WHERE session_id = ?", arguments: [sessionID])
         }
